455112a8f40e6cbdbe547e9d764029de8fea7512
lsakee <93514333+lsakee@users.noreply.github.com>
2023-12-21 18:17:53 +0900

    [feat] : #2 build logic base set

---
+++ build-logic/convention/src/main/java/com/linkmind/convention/Const.kt
@@ -0,0 +1,11 @@
+package com.linkmind.convention
+
+import org.gradle.api.JavaVersion
+
+object Const {
+  const val compileSdk = 34
+  const val minSdk = 24
+  const val targetSdk = 34
+  const val JDK_VERSION = 18
+  val JAVA_VERSION = JavaVersion.VERSION_18
+}
---
+++ build-logic/convention/src/main/java/com/linkmind/convention/KotlinAndroid.kt
@@ -0,0 +1,53 @@
+package com.linkmind.convention
+
+import org.gradle.api.Project
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.plugins.ExtensionAware
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+
+internal fun Project.configureKotlinAndroid(
+  commonExtension: CommonExtension<*, *, *, *, *>,
+) {
+  commonExtension.apply {
+    compileSdk = Const.compileSdk
+
+    defaultConfig {
+      minSdk = Const.minSdk
+
+      testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+      vectorDrawables.useSupportLibrary = true
+    }
+
+    compileOptions {
+      sourceCompatibility = Const.JAVA_VERSION
+      targetCompatibility = Const.JAVA_VERSION
+    }
+
+    kotlinOptions {
+      jvmTarget = Const.JAVA_VERSION.toString()
+    }
+
+    buildTypes {
+      getByName("debug") {
+        proguardFiles(
+          getDefaultProguardFile("proguard-android.txt"),
+          "proguard-debug.pro",
+        )
+      }
+
+      getByName("release") {
+        isMinifyEnabled = true
+        proguardFiles(
+          getDefaultProguardFile("proguard-android.txt"),
+          "proguard-rules.pro",
+        )
+      }
+    }
+  }
+}
+
+internal fun CommonExtension<*, *, *, *, *>.kotlinOptions(
+  block: KotlinJvmOptions.() -> Unit,
+) {
+  (this as ExtensionAware).extensions.configure("kotlinOptions", block)
+}
