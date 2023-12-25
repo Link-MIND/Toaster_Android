96e72f7a4f65c456b8d69fc8c783775c7eb1511d
lsakee <93514333+lsakee@users.noreply.github.com>
2023-12-25 20:44:15 +0900

    [feat] : #2 build logic app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,39 +1,16 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-//  alias(libs.plugins.linkmind.android.library)
   alias(libs.plugins.linkmind.android.application)
   alias(libs.plugins.linkmind.android.hilt)
 }
 
 android {
   namespace = "org.sopt.linkmind"
-  compileSdk = 34
 
   defaultConfig {
     applicationId = "org.sopt.linkmind"
-    minSdk = 24
-    targetSdk = 34
     versionCode = 1
     versionName = "1.0"
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro",
-      )
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-  kotlinOptions {
-    jvmTarget = "17"
   }
 }
 
