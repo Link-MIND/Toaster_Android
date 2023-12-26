2c18e63b8c7e23d0fdcfc2c82b1094121e72d0d2
lsakee <93514333+lsakee@users.noreply.github.com>
2023-12-26 19:55:59 +0900

    [add] : #2 core-auth

---
+++ core/core-auth/build.gradle.kts
@@ -0,0 +1,11 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.linkmind.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "org.sopt.core_auth"
+}
+
+
--- core/core-auth/src/androidTest/java/org/sopt/core_auth/ExampleInstrumentedTest.kt
+++ core/core-auth/src/androidTest/java/org/sopt/core_auth/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package org.sopt.core_common
+package org.sopt.core_auth
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.sopt.core_common.test", appContext.packageName)
+        assertEquals("org.sopt.core_auth.test", appContext.packageName)
     }
 }
