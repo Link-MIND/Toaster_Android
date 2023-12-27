df7d19bc2e0370b0d7986b5b036f46c1ed7a4f43
lsakee <93514333+lsakee@users.noreply.github.com>
2023-12-27 10:16:18 +0900

    [add] : #2 network module

---
+++ core/core-network/src/main/java/org/sopt/core_network/interceptor/AuthenticationIntercept.kt
@@ -0,0 +1,12 @@
+package org.sopt.core_network.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+internal class AuthenticationInterceptor(
+) : Interceptor {
+  override fun intercept(chain: Interceptor.Chain): Response {
+    TODO("Not yet implemented")
+  }
+
+}
