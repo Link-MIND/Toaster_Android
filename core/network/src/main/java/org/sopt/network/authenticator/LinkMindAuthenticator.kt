ab976b66f89bf8fbf196654346654b1776ef8d63
lsakee <93514333+lsakee@users.noreply.github.com>
2023-12-28 02:27:18 +0900

    [chore] : #2 detekt

--- core/network/src/main/java/org/sopt/network/authenticator/LinkMindAuthenticator.kt
+++ core/network/src/main/java/org/sopt/network/authenticator/LinkMindAuthenticator.kt
@@ -5,7 +5,7 @@ import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
 
-internal class LinkMindAuthenticator() : Authenticator {
+internal class LinkMindAuthenticator : Authenticator {
   override fun authenticate(route: Route?, response: Response): Request? {
     TODO("Not yet implemented")
   }
--- core/network/src/main/java/org/sopt/network/interceptor/AuthenticationIntercept.kt
+++ core/network/src/main/java/org/sopt/network/interceptor/AuthenticationIntercept.kt
@@ -3,7 +3,7 @@ package org.sopt.network.interceptor
 import okhttp3.Interceptor
 import okhttp3.Response
 
-internal class AuthenticationIntercept() : Interceptor {
+internal class AuthenticationIntercept : Interceptor {
   override fun intercept(chain: Interceptor.Chain): Response {
     TODO("Not yet implemented")
   }
