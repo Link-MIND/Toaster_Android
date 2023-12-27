804a5df649f164747981ec4db1fc5bbea21977dd
lsakee <93514333+lsakee@users.noreply.github.com>
2023-12-27 11:10:38 +0900

    [add] : #2 domain

--- build-logic/convention/src/main/java/com/linkmind/convention/Const.kt
+++ build-logic/convention/src/main/java/com/linkmind/convention/Const.kt
@@ -6,6 +6,6 @@ object Const {
   const val compileSdk = 34
   const val minSdk = 24
   const val targetSdk = 34
-  const val JDK_VERSION = 18
-  val JAVA_VERSION = JavaVersion.VERSION_18
+  const val JDK_VERSION = 17
+  val JAVA_VERSION = JavaVersion.VERSION_17
 }
