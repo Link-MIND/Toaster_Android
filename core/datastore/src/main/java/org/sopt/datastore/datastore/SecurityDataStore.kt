915850e9cf8974342bd014eb0cce082b780e1371
Sangwook123 <operawook@catholic.ac.kr>
2023-12-31 05:15:56 +0900

    [chore] #7 datastore devicetoken logic

--- core/datastore/src/main/java/org/sopt/datastore/datastore/SecurityDataStore.kt
+++ core/datastore/src/main/java/org/sopt/datastore/datastore/SecurityDataStore.kt
@@ -19,8 +19,14 @@ interface SecurityDataStore {
   @SetValue(KEY_REFRESHTOKEN)
   suspend fun setRefreshToken(string: String)
 
+  @GetValue(KEY_DEVICETOKEN)
+  fun flowDeviceToken(): Flow<String>
+
+  @SetValue(KEY_DEVICETOKEN)
+  suspend fun setDeviceToken(string: String)
   companion object {
     const val KEY_ACCESSTOKEN = "key-accesstoken"
     const val KEY_REFRESHTOKEN = "key-refreshtoken"
+    const val KEY_DEVICETOKEN = "key-devicetoken"
   }
 }
