e9a1086c58504cf670c7e8e1eb9a00813b847732
Sangwook123 <operawook@catholic.ac.kr>
2023-12-30 17:51:36 +0900

    [feat] #3 domain repository, usecase

---
+++ domain/linkminddomain/src/main/java/org/sopt/linkminddomain/usecase/DummyUseCase.kt
@@ -0,0 +1,9 @@
+package org.sopt.linkminddomain.usecase
+
+import org.sopt.linkminddomain.repository.DummyRepository
+
+class DummyUseCase(
+  private val dummyRepository: DummyRepository
+) {
+  suspend operator fun invoke() = dummyRepository.getDummy()
+}
