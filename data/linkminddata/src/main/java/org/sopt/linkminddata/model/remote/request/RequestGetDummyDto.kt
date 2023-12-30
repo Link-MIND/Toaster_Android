ffcda21783711044d79e3a06f5ca2e103eaacff4
Sangwook123 <operawook@catholic.ac.kr>
2023-12-30 17:49:02 +0900

    [feat] #3 dummy dto

---
+++ data/linkminddata/src/main/java/org/sopt/linkminddata/model/remote/request/RequestGetDummyDto.kt
@@ -0,0 +1,10 @@
+package org.sopt.linkminddata.model.remote.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RequestGetDummyDto (
+  @SerialName("dummy")
+  val request : String
+)
