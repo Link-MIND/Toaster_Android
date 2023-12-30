4d4ffd824913ca88708ba4e4d2b138355c173bf0
Sangwook123 <operawook@catholic.ac.kr>
2023-12-30 17:45:26 +0900

    [chore] #3 timber

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
   implementation(libs.appcompat)
   implementation(libs.material)
   implementation(libs.constraintlayout)
+  implementation(libs.timber)
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.test.ext.junit)
   androidTestImplementation(libs.espresso.core)
