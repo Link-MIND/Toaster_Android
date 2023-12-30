13154b639e6b6016cf7a8b52760908e41f88c41d
Sangwook123 <operawook@catholic.ac.kr>
2023-12-30 17:49:02 +0900

    [feat] #3 dummy dto

---
+++ data/linkminddata/src/main/java/org/sopt/linkminddata/model/local/Dummy.kt
@@ -0,0 +1,9 @@
+package org.sopt.linkminddata.model.local
+
+import org.sopt.linkminddomain.entity.DummyEntity
+
+data class Dummy(
+  val string: String
+){
+  fun toDomainModel() : DummyEntity = DummyEntity(this.string)
+}
