f158db1858d9dcea14a3f83d5c4540a0b764c443
Sangwook123 <operawook@catholic.ac.kr>
2023-12-31 21:20:37 +0900

    [feat] #7 auth domain repository

---
+++ core/auth/src/main/java/org/sopt/auth/repository/AuthRepository.kt
@@ -0,0 +1,13 @@
+package org.sopt.auth.repository
+
+import org.sopt.auth.model.Auth
+import org.sopt.auth.model.Token
+import org.sopt.auth.model.UserData
+
+interface AuthRepository {
+  suspend fun authenticate(auth : Auth) : Result<Pair<Token,UserData>?>
+  suspend fun signout() : Result<Unit>
+  suspend fun withdraw() : Result<Unit>
+  suspend fun saveToken(token: Token)
+  suspend fun clearToken()
+}
