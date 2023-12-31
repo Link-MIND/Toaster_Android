5fbe02b2ffff6cbafe8a763672e976cd322453f3
Sangwook123 <operawook@catholic.ac.kr>
2023-12-31 21:21:21 +0900

    [feat] #7 auth dto

---
+++ core/authimpl/src/main/java/org/sopt/authimpl/model/request/RequestPostAuthDto.kt
@@ -0,0 +1,21 @@
+package org.sopt.authimpl.model.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.sopt.auth.model.Auth
+
+@Serializable
+data class RequestPostAuthDto(
+  @SerialName("socialType")
+  val socialType: String,
+  @SerialName("fcmToken")
+  val fcmToken: String
+){
+  companion object{
+    fun Auth.toDataModel() =
+      RequestPostAuthDto(
+        this.socialType,
+        this.deviceToken
+      )
+  }
+}
---
+++ core/authimpl/src/main/java/org/sopt/authimpl/model/response/ResponsePostAuthDto.kt
@@ -0,0 +1,35 @@
+package org.sopt.authimpl.model.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.sopt.auth.model.Token
+import org.sopt.auth.model.UserData
+
+@Serializable
+data class ResponsePostAuthDto(
+  @SerialName("userId")
+  val userId: Int,
+  @SerialName("accessToken")
+  val accessToken: String,
+  @SerialName("refreshToken")
+  val refreshToken: String,
+  @SerialName("fcmToken")
+  val fcmToken: String,
+  @SerialName("isRegistered")
+  val isRegistered: Boolean,
+  @SerialName("fcmIsAllowed")
+  val fcmIsAllowed: Boolean,
+){
+  fun toDomainModel() = Pair(
+    Token(
+      this.accessToken,
+      this.refreshToken,
+      this.fcmToken
+    ),
+    UserData(
+      this.userId,
+      this.isRegistered,
+      this.fcmIsAllowed
+    )
+  )
+}
