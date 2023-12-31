6c4a1ad5d62185d73f5dbde2124a692c60e6bc99
Sangwook123 <operawook@catholic.ac.kr>
2023-12-31 21:21:54 +0900

    [feat] #7 auth service

---
+++ core/authimpl/src/main/java/org/sopt/authimpl/sourceimpl/remote/api/AuthService.kt
@@ -0,0 +1,24 @@
+package org.sopt.authimpl.sourceimpl.remote.api
+
+import org.sopt.authimpl.model.request.RequestPostAuthDto
+import org.sopt.authimpl.model.response.ResponsePostAuthDto
+import org.sopt.authimpl.model.response.ResponsePostSignOutDto
+import org.sopt.network.model.response.base.BaseResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface AuthService {
+  @POST("auth")
+  suspend fun postLogin(
+    @Header("Authorization") socialAccessToken: String,
+    @Body requestLoginDto: RequestPostAuthDto
+  ): BaseResponse<ResponsePostAuthDto>
+
+  @POST("auth/sign-out")
+  suspend fun postSignOut(): ResponsePostSignOutDto
+
+  @DELETE("auth/withdraw")
+  suspend fun deleteUser(): BaseResponse<Unit>
+}
