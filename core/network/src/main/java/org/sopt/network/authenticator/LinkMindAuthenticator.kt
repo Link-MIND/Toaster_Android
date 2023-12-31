98e46e5fdd6ef43102c1cfd923c86b853a6ab62e
Sangwook123 <operawook@catholic.ac.kr>
2023-12-31 21:17:54 +0900

    [feat] #7 authenticator

--- core/network/src/main/java/org/sopt/network/authenticator/LinkMindAuthenticator.kt
+++ core/network/src/main/java/org/sopt/network/authenticator/LinkMindAuthenticator.kt
@@ -1,12 +1,48 @@
 package org.sopt.network.authenticator
 
+import android.content.Context
+import com.jakewharton.processphoenix.ProcessPhoenix
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.runBlocking
 import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
+import org.sopt.common.intentprovider.IntentProvider
+import org.sopt.datastore.datastore.SecurityDataStore
+import org.sopt.network.service.TokenRefreshService
+import javax.inject.Inject
 
-internal class LinkMindAuthenticator : Authenticator {
+class LinkMindAuthenticator @Inject constructor(
+  private val dataStore: SecurityDataStore,
+  private val tokenRefreshService: TokenRefreshService,
+  @ApplicationContext private val context: Context,
+  private val intentProvider: IntentProvider
+) : Authenticator {
   override fun authenticate(route: Route?, response: Response): Request? {
-    TODO("Not yet implemented")
+    if (response.code == 401) {
+      val newTokens = runCatching {
+        runBlocking {
+          tokenRefreshService.postAuthRefresh()
+        }
+      }.onSuccess {
+        runBlocking {
+          dataStore.apply {
+            setAccessToken(it.data?.accessToken ?: "")
+            setRefreshToken(it.data?.refreshToken ?: "")
+          }
+        }
+      }.onFailure {
+        runBlocking {
+          dataStore.clearAll()
+        }
+        ProcessPhoenix.triggerRebirth(context, intentProvider.getAuthIntent())
+      }.getOrThrow()
+
+      return response.request.newBuilder()
+        .header("accessToken", newTokens.data?.accessToken ?: "")
+        .build()
+    }
+    return null
   }
 }
