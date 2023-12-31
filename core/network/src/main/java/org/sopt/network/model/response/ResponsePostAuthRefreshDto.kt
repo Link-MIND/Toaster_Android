8e50f6647498e94ac00483d7e5bd7883db9dfbd8
Sangwook123 <operawook@catholic.ac.kr>
2023-12-31 21:16:29 +0900

    [feat] #7 refresh dto

---
+++ core/network/src/main/java/org/sopt/network/model/response/ResponsePostAuthRefreshDto.kt
@@ -0,0 +1,12 @@
+package org.sopt.network.model.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponsePostAuthRefreshDto(
+  @SerialName("accessToken")
+  val accessToken: String,
+  @SerialName("refreshToken")
+  val refreshToken: String
+)
