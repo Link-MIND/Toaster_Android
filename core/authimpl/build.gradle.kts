381c18ce7aafe0828ad3f9bdb303e9cae962226a
Sangwook123 <operawook@catholic.ac.kr>
2023-12-31 21:20:52 +0900

    [add] #7 authimpl dependencies

--- core/authimpl/build.gradle.kts
+++ core/authimpl/build.gradle.kts
@@ -1,7 +1,9 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
   alias(libs.plugins.linkmind.android.library)
+  alias(libs.plugins.linkmind.android.hilt)
   alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -9,4 +11,9 @@ android {
 }
 
 dependencies {
+  implementation(projects.core.network)
+  implementation(projects.core.auth)
+  implementation(projects.core.datastore)
+  implementation(libs.bundles.coroutine)
+  implementation(libs.retrofit.kotlin.serialization)
 }
