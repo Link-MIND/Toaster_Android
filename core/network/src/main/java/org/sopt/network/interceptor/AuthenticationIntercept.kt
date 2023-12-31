a332760b2d9c40e0005a385b658a198cf085a3f0
Sangwook123 <operawook@catholic.ac.kr>
2023-12-31 21:17:42 +0900

    [feat] #7 authentication interceptor

--- core/network/src/main/java/org/sopt/network/interceptor/AuthenticationIntercept.kt
+++ core/network/src/main/java/org/sopt/network/interceptor/AuthenticationIntercept.kt
@@ -1,10 +1,44 @@
 package org.sopt.network.interceptor
 
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
+import org.sopt.core.network.BuildConfig.BASE_URL
+import org.sopt.datastore.datastore.SecurityDataStore
+import javax.inject.Inject
 
-internal class AuthenticationIntercept : Interceptor {
+class AuthenticationIntercept @Inject constructor(
+  private val dataStore: SecurityDataStore,
+) : Interceptor {
   override fun intercept(chain: Interceptor.Chain): Response {
-    TODO("Not yet implemented")
+    val originalRequest = chain.request()
+    val authRequest = handleRequest(originalRequest)
+    return chain.proceed(authRequest)
   }
+
+  private fun handleRequest(originalRequest: Request) =
+    when {
+      originalRequest.url.encodedPath.contains("auth") -> {
+        when {
+          originalRequest.url.encodedPath.contains("withdraw") || originalRequest.url.encodedPath.contains("out")  -> {
+            originalRequest.accessTokenBuilder()
+          }
+          originalRequest.url.encodedPath.contains("token") -> {
+            originalRequest.refreshTokenBuilder()
+          }
+          else -> {
+            originalRequest
+          }
+        }
+      }
+      else -> originalRequest.accessTokenBuilder()
+    }
+
+  private fun Request.accessTokenBuilder() =
+    this.newBuilder().addHeader("accessToken", runBlocking { dataStore.flowAccessToken().first() }).build()
+
+  private fun Request.refreshTokenBuilder() =
+    this.newBuilder().addHeader("refreshToken", runBlocking { dataStore.flowRefreshToken().first() }).build()
 }
