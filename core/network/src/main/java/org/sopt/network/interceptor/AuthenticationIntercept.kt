5bef62b29755451f389ef7f0f9abac878560a0f8
Sangwook123 <operawook@catholic.ac.kr>
2024-01-01 21:02:18 +0900

    [feat] #7 authintercept 404처리

--- core/network/src/main/java/org/sopt/network/interceptor/AuthenticationIntercept.kt
+++ core/network/src/main/java/org/sopt/network/interceptor/AuthenticationIntercept.kt
@@ -1,19 +1,32 @@
 package org.sopt.network.interceptor
 
+import android.content.Context
+import android.util.Log
+import com.jakewharton.processphoenix.ProcessPhoenix
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
+import org.sopt.common.intentprovider.IntentProvider
 import org.sopt.datastore.datastore.SecurityDataStore
 import javax.inject.Inject
 
 class AuthenticationIntercept @Inject constructor(
   private val dataStore: SecurityDataStore,
+  @ApplicationContext private val context: Context,
+  private val intentProvider: IntentProvider,
 ) : Interceptor {
   override fun intercept(chain: Interceptor.Chain): Response {
     val originalRequest = chain.request()
     val authRequest = handleRequest(originalRequest)
+    val response = chain.proceed(authRequest)
+    if (response.code == CODE_INVALID_USER) {
+      runBlocking { dataStore.setAutoLogin(false) }
+      ProcessPhoenix.triggerRebirth(context, intentProvider.getAuthIntent())
+    }
+    response.close()
     return chain.proceed(authRequest)
   }
 
@@ -24,15 +37,20 @@ class AuthenticationIntercept @Inject constructor(
           originalRequest.url.encodedPath.contains("withdraw") || originalRequest.url.encodedPath.contains("out") -> {
             originalRequest.accessTokenBuilder()
           }
+
           originalRequest.url.encodedPath.contains("token") -> {
             originalRequest.refreshTokenBuilder()
           }
+
           else -> {
             originalRequest
           }
         }
       }
-      else -> originalRequest.accessTokenBuilder()
+
+      else -> {
+        originalRequest.accessTokenBuilder()
+      }
     }
 
   private fun Request.accessTokenBuilder() =
@@ -40,4 +58,8 @@ class AuthenticationIntercept @Inject constructor(
 
   private fun Request.refreshTokenBuilder() =
     this.newBuilder().addHeader("refreshToken", runBlocking { dataStore.flowRefreshToken().first() }).build()
+
+  companion object {
+    private const val CODE_INVALID_USER = 404
+  }
 }
