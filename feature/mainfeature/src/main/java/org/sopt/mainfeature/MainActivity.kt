f42592b3af68981fa9732e043d11df27b9ed4fcc
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-01 19:43:18 +0900

    [feat] : #6  jetpack nav main

--- feature/mainfeature/src/main/java/org/sopt/mainfeature/MainActivity.kt
+++ feature/mainfeature/src/main/java/org/sopt/mainfeature/MainActivity.kt
@@ -1,11 +1,51 @@
 package org.sopt.mainfeature
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import org.sopt.mainfeature.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  private var mBinding: ActivityMainBinding? = null
+  private val binding get() = mBinding!!
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+    setContentView(binding.root)
+    initView()
   }
+
+  private fun initView() {
+    val navController =
+      supportFragmentManager.findFragmentById(R.id.fcv_main)?.findNavController()!!
+
+    with(binding) {
+      bnvMain.itemIconTintList = null
+      navController?.let { NavController ->
+        bnvMain.setupWithNavController(NavController)
+      }
+
+    }
+
+    setBottomVisible(navController)
+  }
+
+  private fun setBottomVisible(navController: NavController) {
+    navController.addOnDestinationChangedListener { _, destination, _ ->
+      binding.bnvMain.visibility = if (destination.id in listOf(
+          R.id.navigation_home,
+          R.id.navigation_clip,
+          R.id.navigation_timer,
+          R.id.navigation_my,
+        )
+      ) View.VISIBLE else View.GONE
+
+    }
+  }
+
 }
