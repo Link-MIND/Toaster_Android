61ccc7279a0f436bd4d70f92aa0bc3d48e921077
Sangwook123 <operawook@catholic.ac.kr>
2024-01-01 21:03:14 +0900

    [del] #7 main example code

--- feature/mainfeature/src/main/java/org/sopt/mainfeature/MainActivity.kt
+++ feature/mainfeature/src/main/java/org/sopt/mainfeature/MainActivity.kt
@@ -3,33 +3,17 @@ package org.sopt.mainfeature
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import org.sopt.mainfeature.databinding.ActivityMainBinding
-import org.sopt.oauthdomain.interactor.OAuthInteractor
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
   lateinit var binding: ActivityMainBinding
-
-  @Inject
-  lateinit var kakaoAuthInteractor: OAuthInteractor
 
   private val viewModel: MainViewModel by viewModels()
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
-
-    binding.tvMain.setOnClickListener {
-      lifecycleScope.launch {
-        kakaoAuthInteractor.loginByKakao().onSuccess {
-          viewModel.login()
-        }.onFailure {
-        }
-      }
-    }
   }
 }
