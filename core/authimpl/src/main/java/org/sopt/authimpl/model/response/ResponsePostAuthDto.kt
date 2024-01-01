7fdd159b3554b8977f52009083cf0dc833ab2d60
Sangwook123 <operawook@catholic.ac.kr>
2024-01-01 20:59:47 +0900

    [chore] #7 dto 수정

--- core/authimpl/src/main/java/org/sopt/authimpl/model/response/ResponsePostAuthDto.kt
+++ core/authimpl/src/main/java/org/sopt/authimpl/model/response/ResponsePostAuthDto.kt
@@ -17,7 +17,7 @@ data class ResponsePostAuthDto(
   val fcmToken: String,
   @SerialName("isRegistered")
   val isRegistered: Boolean,
-  @SerialName("fcmIsAllowed")
+  @SerialName("FcmIsAllowed")
   val fcmIsAllowed: Boolean,
 ) {
   fun toDomainModel() = Pair(
