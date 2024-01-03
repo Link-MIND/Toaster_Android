674d28d0a0fea1105530887863959f9200f143fe
Choi Min Young <codingmy@naver.com>
2024-01-03 23:55:44 +0900

    [refactor] #12 BindingFragment viewBinding으로 전환

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -9,5 +9,6 @@ android {
 
   buildFeatures {
     dataBinding = true
+    viewBinding = true
   }
 }
--- core/ui/src/main/java/org/sopt/ui/base/BindingFragment.kt
+++ core/ui/src/main/java/org/sopt/ui/base/BindingFragment.kt
@@ -4,28 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
 
-abstract class BindingFragment<T : ViewDataBinding>(
-  @LayoutRes private val layoutRes: Int,
-) : Fragment() {
+abstract class BindingFragment<T : ViewBinding> : Fragment() {
   private var _binding: T? = null
-  protected val binding get() = requireNotNull(_binding) {
-  }
+  protected val binding
+    get() = requireNotNull(_binding) {
+    }
 
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?,
   ): View? {
-    _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
-    binding.lifecycleOwner = viewLifecycleOwner
+    _binding = getFragmentBinding(inflater, container)
     return binding.root
   }
 
+  abstract fun getFragmentBinding(inflater: LayoutInflater, container: ViewGroup?): T
   override fun onDestroyView() {
     _binding = null
     super.onDestroyView()
