54100014cf4c380bdf6cbeb4d3c43aa6119591ff
Sangwook123 <operawook@catholic.ac.kr>
2024-01-03 22:21:57 +0900

    [ui] #18 타이머 프래그먼트

--- feature/mainfeature/src/main/java/org/sopt/mainfeature/timer/TimerFragment.kt
+++ feature/mainfeature/src/main/java/org/sopt/mainfeature/timer/TimerFragment.kt
@@ -4,58 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import org.sopt.mainfeature.R
+import org.sopt.mainfeature.databinding.FragmentTimerBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TimerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TimerFragment : Fragment() {
-  // TODO: Rename and change types of parameters
-  private var param1: String? = null
-  private var param2: String? = null
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    arguments?.let {
-      param1 = it.getString(ARG_PARAM1)
-      param2 = it.getString(ARG_PARAM2)
-    }
-  }
-
+  lateinit var binding : FragmentTimerBinding
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?,
   ): View? {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_timer, container, false)
+    binding = FragmentTimerBinding.inflate(layoutInflater)
+    return binding.root
   }
 
-  companion object {
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TimerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    @JvmStatic
-    fun newInstance(param1: String, param2: String) =
-      TimerFragment().apply {
-        arguments = Bundle().apply {
-          putString(ARG_PARAM1, param1)
-          putString(ARG_PARAM2, param2)
-        }
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    binding.clTimerPermission.setOnClickListener {
+      if(binding.clTimerPermissionOff.isVisible){
+        binding.clTimerPermissionOff.isGone = true
+      }else{
+        binding.clTimerPermissionOff.isGone = false
       }
+    }
   }
 }
