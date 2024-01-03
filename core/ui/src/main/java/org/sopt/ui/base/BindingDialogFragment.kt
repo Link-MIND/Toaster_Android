e4cfaa5755488dd5ff75f0924ceade147693e6ba
Choi Min Young <codingmy@naver.com>
2024-01-04 00:11:48 +0900

    [refactor] #12 BindingDialogFragment viewBinding으로 전환

--- core/ui/src/main/java/org/sopt/ui/base/BindingDialogFragment.kt
+++ core/ui/src/main/java/org/sopt/ui/base/BindingDialogFragment.kt
@@ -5,14 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
-import androidx.annotation.LayoutRes
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.DialogFragment
+import androidx.viewbinding.ViewBinding
 
-abstract class BindingDialogFragment<T : ViewDataBinding>(
-  @LayoutRes val layoutRes: Int,
-) : DialogFragment() {
+abstract class BindingDialogFragment<T : ViewBinding> : DialogFragment() {
   private var _binding: T? = null
   protected val binding get() = requireNotNull(_binding) { { "binding object is not initialized" } }
 
@@ -31,14 +27,15 @@ abstract class BindingDialogFragment<T : ViewDataBinding>(
     container: ViewGroup?,
     savedInstanceState: Bundle?,
   ): View {
-    _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+    _binding = getFragmentBinding(inflater, container)
     return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    binding.lifecycleOwner = viewLifecycleOwner
   }
+
+  abstract fun getFragmentBinding(inflater: LayoutInflater, container: ViewGroup?): T
 
   override fun onDestroyView() {
     _binding = null
