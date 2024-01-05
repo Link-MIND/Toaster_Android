aacfbe11584fc70cda1ed57af7ef61219539803e
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-06 07:13:09 +0900

    [feat] : #41 build set

---
+++ feature/login/build.gradle.kts
@@ -0,0 +1,28 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+  alias(libs.plugins.linkmind.plugin.feature)
+  alias(libs.plugins.kotlin.android)
+}
+
+android {
+  namespace = "org.sopt.login"
+  buildFeatures {
+    viewBinding = true
+  }
+}
+
+dependencies {
+  implementation(projects.feature.maincontainer)
+  implementation(projects.core.datastore)
+  implementation(projects.core.auth)
+  implementation(projects.domain.linkminddomain)
+  implementation(projects.domain.oauthdomain)
+  implementation(projects.core.common)
+  implementation(projects.core.ui)
+  implementation(libs.core.ktx)
+  implementation(libs.appcompat)
+  implementation(libs.material)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.test.ext.junit)
+  androidTestImplementation(libs.espresso.core)
+}
