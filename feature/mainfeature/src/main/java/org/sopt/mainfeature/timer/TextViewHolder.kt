bf14263e5bbca929c134fac1d3ed67e20a355c05
Sangwook123 <operawook@catholic.ac.kr>
2024-01-06 06:53:04 +0900

    [feat] #18 text adapter

---
+++ feature/mainfeature/src/main/java/org/sopt/mainfeature/timer/TextViewHolder.kt
@@ -0,0 +1,22 @@
+package org.sopt.mainfeature.timer
+
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.mainfeature.R
+import org.sopt.mainfeature.databinding.ItemNumberPickerBinding
+import org.sopt.mainfeature.timer.dummymodel.PickerItem
+
+class TextViewHolder(
+  val binding: ItemNumberPickerBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+  fun onBind(data: PickerItem?) {
+    if (data == null) return
+    with(binding) {
+      if (data.isSelected) {
+        tvText.setTextAppearance(R.style.Typography_suit_bold_18)
+      } else {
+        tvText.setTextAppearance(R.style.Typography_suit_regular_16)
+      }
+      tvText.text = data.text
+    }
+  }
+}
