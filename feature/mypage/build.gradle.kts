4ccf6c5b374179b9d2afaf739e8ed8d24338afde
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-06 22:46:57 +0900

    [feat] : #41 design-system 모듈 dependency

--- feature/mypage/build.gradle.kts
+++ feature/mypage/build.gradle.kts
@@ -12,4 +12,5 @@ android {
 }
 
 dependencies {
+  implementation(projects.feature.mainfeature)
 }
--- feature/timer/build.gradle.kts
+++ feature/timer/build.gradle.kts
@@ -12,5 +12,5 @@ android {
 }
 
 dependencies {
-
+  implementation(projects.feature.mainfeature)
 }
