d7b6044fe2b31b3f590f05f9eb7f02040539ae4c
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-06 18:51:32 +0900

    [feat] : #41 core ui build set

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,14 +1,26 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-  alias(libs.plugins.linkmind.plugin.feature)
-  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.linkmind.android.library)
+  alias(libs.plugins.linkmind.android.hilt)
 }
 
 android {
   namespace = "org.sopt.ui"
 
   buildFeatures {
-    dataBinding = true
     viewBinding = true
   }
+}
+
+dependencies{
+    implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.constraintlayout)
+  implementation(libs.androidx.activity.ktx)
+  implementation(libs.androidx.fragment.ktx)
+  implementation(libs.androidx.recyclerview)
+  implementation(libs.google.material)
+  implementation(libs.timber)
+  implementation(libs.bundles.androidx.lifecycle)
+  implementation(libs.bundles.coroutine)
 }
