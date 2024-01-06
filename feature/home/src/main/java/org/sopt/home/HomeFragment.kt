93b62941bb520af8e2bd3126211e6f4bd57220b1
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-07 08:03:59 +0900

    [feat] : #45 home clip rv

--- feature/home/src/main/java/org/sopt/home/HomeFragment.kt
+++ feature/home/src/main/java/org/sopt/home/HomeFragment.kt
@@ -1,18 +1,32 @@
 package org.sopt.home
 
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import org.sopt.home.databinding.FragmentHomeBinding
+import org.sopt.ui.DeepLinkUtil
 import org.sopt.ui.base.BindingFragment
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>({ FragmentHomeBinding.inflate(it) }) {
+
+  private lateinit var homeClipAdapter: HomeClipAdapter
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     binding.root.setOnClickListener {
-      val uri = Uri.parse("featureTimer://fragmentExample")
-      findNavController().navigate(uri)
+      val (request, navOptions) = DeepLinkUtil.getNavRequestNotPopUpAndOption(
+        "featureTimer://fragmentExample",
+      )
+//      val (request, navOptions) = DeepLinkUtil.getNavRequestPopUpAndOption(
+//        findNavController().graph.id,
+//        false,
+//        "featureTimer://fragmentExample",
+//      )
+      findNavController().navigate(request, navOptions)
     }
+    homeClipAdapter = HomeClipAdapter(onClickItemClip = {})
+    val list = listOf<ClipDummy>(ClipDummy("test", 3), ClipDummy("test", 3), ClipDummy("test", 3))
+    homeClipAdapter.submitList(list)
+    binding.rvHomeClip.adapter = homeClipAdapter
+
   }
 }
