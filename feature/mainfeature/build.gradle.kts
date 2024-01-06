b66234f09d60b4d4eb614e887da94a3e79a036db
CHANGEL <107169027+CHANGEL1004@users.noreply.github.com>
2024-01-06 18:26:36 +0900

    [chore] : #37 라이브러리 변경

--- feature/mainfeature/build.gradle.kts
+++ feature/mainfeature/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
   implementation(libs.appcompat)
   implementation(libs.material)
   implementation(libs.constraintlayout)
+  implementation(libs.androidx.preference)
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.test.ext.junit)
   androidTestImplementation(libs.espresso.core)
