3a29bc20d497a74050a32701613567a31b86c106
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-06 23:07:41 +0900

    [delete] : #41 불필요 파일 제거

--- feature/mainfeature/src/main/java/org/sopt/mainfeature/MainViewModel.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.mainfeature
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel()
