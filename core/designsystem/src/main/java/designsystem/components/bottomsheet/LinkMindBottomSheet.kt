75f0135283485c08ce73d9e6d3dd725b9306a446
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-07 18:01:29 +0900

    [feat] : #45 component 힌트 변경 메소드 추가

--- core/designsystem/src/main/java/designsystem/components/bottomsheet/LinkMindBottomSheet.kt
+++ core/designsystem/src/main/java/designsystem/components/bottomsheet/LinkMindBottomSheet.kt
@@ -38,6 +38,10 @@ class LinkMindBottomSheet(context: Context) {
     }
   }
 
+
+  fun setBottomSheetHint(@StringRes textId: Int){
+    binding.etvBottomSheet.editText.setHint(textId)
+  }
   fun bottomSheetConfirmBtnClick(onClick: () -> Unit) {
     binding.btnBottomSheet.btnClick {
       onClick()
