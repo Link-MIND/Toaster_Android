f81ea0891824e47cf0be360bf3fe4cd23844ad72
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-07 20:28:09 +0900

    [feat] : #45 기능

--- feature/savelink/src/main/java/org/sopt/savelink/SaveLinkFragment.kt
+++ feature/savelink/src/main/java/org/sopt/savelink/SaveLinkFragment.kt
@@ -1,9 +1,9 @@
 package org.sopt.savelink
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import androidx.navigation.fragment.findNavController
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import designsystem.components.button.state.LinkMIndFullWidthButtonState
 import org.sopt.savelink.databinding.FragmentSaveLinkBinding
 import org.sopt.ui.base.BindingFragment
@@ -12,12 +12,31 @@ class SaveLinkFragment : BindingFragment<FragmentSaveLinkBinding>({ FragmentSave
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     binding.etvSaveCopyLink.apply {
-      onClickTextClear {  }
-      if(checkTextLength(15)) Log.d("test","test")
+      onClickTextClear { }
+      throttleAfterTextChanged {
+        if (checkTextLength(15)) {
+          binding.tvSaveLinkError.isVisible = true
+        } else {
+          binding.tvSaveLinkError.isGone = true
+          binding.btnSaveLinkNext.state = LinkMIndFullWidthButtonState.ENABLE_BLACK
+          binding.btnSaveLinkNext.btnClick {
+            with(binding) {
+              btnSaveLinkNext.state = LinkMIndFullWidthButtonState.DISABLE
+              tvSaveLinkSubTitle.isVisible = true
+              etvSaveCopyLinkTitle.isVisible = true
+            }
+          }
+        }
+      }
     }
-
+    binding.etvSaveCopyLinkTitle.apply {
+      onClickTextClear { }
+      if (checkTextLength(15)) {
+        binding.tvSaveLinkError.isVisible = true
+      }
+    }
     binding.btnSaveLinkNext.apply {
-      state=LinkMIndFullWidthButtonState.DISABLE
+      state = LinkMIndFullWidthButtonState.DISABLE
     }
 //    binding.root.setOnClickListener {
 //      findNavController().navigateUp()
