d3df3c8ab61c9c2049fe638cdd0777c12bf4c95a
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-07 10:02:05 +0900

    [feat] : #45 코드 정리

--- feature/home/src/main/java/org/sopt/home/HomeFragment.kt
+++ feature/home/src/main/java/org/sopt/home/HomeFragment.kt
@@ -3,6 +3,9 @@ package org.sopt.home
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
+import org.sopt.home.adapter.HomeClipAdapter
+import org.sopt.home.adapter.HomeWeekLinkAdapter
+import org.sopt.home.adapter.HomeWeekRecommendLinkAdapter
 import org.sopt.home.databinding.FragmentHomeBinding
 import org.sopt.ui.DeepLinkUtil
 import org.sopt.ui.base.BindingFragment
@@ -11,6 +14,7 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>({ FragmentHomeBinding.
 
   private lateinit var homeClipAdapter: HomeClipAdapter
   private lateinit var homeWeekLinkAdapter: HomeWeekLinkAdapter
+  private lateinit var homeWeekRecommendLinkAdapter: HomeWeekRecommendLinkAdapter
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     binding.root.setOnClickListener {
@@ -24,21 +28,39 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>({ FragmentHomeBinding.
 //      )
       findNavController().navigate(request, navOptions)
     }
-    initClipAdapter()
+    initAdapter()
     val list = listOf(ClipDummy("1", 1), ClipDummy("2", 2), ClipDummy("3", 3), null)
-    homeClipAdapter.submitList(list)
     val list2 = listOf(
       WeekLinkDummy("1", "www.naver.com", "https://avatars.githubusercontent.com/u/93514333?v=4"),
       WeekLinkDummy("1", "www.naver.com", "https://avatars.githubusercontent.com/u/93514333?v=4"),
       WeekLinkDummy("1", "www.naver.com", "https://avatars.githubusercontent.com/u/93514333?v=4"),
     )
+    homeClipAdapter.submitList(list)
     homeWeekLinkAdapter.submitList(list2)
+    homeWeekRecommendLinkAdapter.submitList(list2)
+  }
+  private fun initAdapter() {
+    setClipAdapter()
+    setWeekLinkAdapter()
+    setWeekRecommendAdapter()
   }
 
-  private fun initClipAdapter() {
+  private fun setClipAdapter() {
     homeClipAdapter = HomeClipAdapter(onClickItemClip = {}, onClickItemClip2 = {})
     binding.rvHomeClip.adapter = homeClipAdapter
+    val spacingClipInPixels = resources.getDimensionPixelSize(R.dimen.spacing_11)
+    binding.rvHomeClip.addItemDecoration(GridSpacingItemDecoration(2, spacingClipInPixels, false))
+  }
+
+  private fun setWeekLinkAdapter() {
     homeWeekLinkAdapter = HomeWeekLinkAdapter(onClickItem = {})
     binding.rvWeekLink.adapter = homeWeekLinkAdapter
+  }
+
+  private fun setWeekRecommendAdapter() {
+    homeWeekRecommendLinkAdapter = HomeWeekRecommendLinkAdapter(onClickItem = {})
+    binding.rvHomeWeekRecommend.adapter = homeWeekRecommendLinkAdapter
+    val spacingWeekRecommendInPixels = resources.getDimensionPixelSize(R.dimen.spacing_12)
+    binding.rvHomeWeekRecommend.addItemDecoration(GridSpacingItemDecoration(3, spacingWeekRecommendInPixels, false))
   }
 }
