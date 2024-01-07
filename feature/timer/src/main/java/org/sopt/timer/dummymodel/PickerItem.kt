f973ab3d1f5c269f9b95daf8174031b072259aa8
Sangwook123 <operawook@catholic.ac.kr>
2024-01-07 12:13:19 +0900

    [chore] #18 ktlintformat

--- feature/timer/src/main/java/org/sopt/timer/dummymodel/PickerItem.kt
+++ feature/timer/src/main/java/org/sopt/timer/dummymodel/PickerItem.kt
@@ -3,7 +3,7 @@ package org.sopt.timer.dummymodel
 data class PickerItem(
   val text: String,
   val isSelected: Boolean,
-){
+) {
   fun convertToText() = this.text.toIntOrNull()?.let {
     if (it < 10) "0${this.text}" else this.text
   } ?: this.text
