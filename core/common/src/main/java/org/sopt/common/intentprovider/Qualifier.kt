f5dd291fade06a3fad14b3325b4d91353e21f25f
Sangwook123 <operawook@catholic.ac.kr>
2024-01-08 06:56:27 +0900

    [add] #64 intent provider qualifier

---
+++ core/common/src/main/java/org/sopt/common/intentprovider/Qualifier.kt
@@ -0,0 +1,12 @@
+package org.sopt.common.intentprovider
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class LOGIN
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MAIN
+
