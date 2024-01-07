8605d51f8eb6dabaf0d524e2e01b528397f09b3c
Sangwook123 <operawook@catholic.ac.kr>
2024-01-08 00:50:38 +0900

    [feat] #57 setting 프래그먼트 구현

--- feature/mypage/src/main/java/org/sopt/mypage/settings/SettingsFragment.kt
+++ feature/mypage/src/main/java/org/sopt/mypage/settings/SettingsFragment.kt
@@ -1,17 +1,37 @@
 package org.sopt.mypage.settings
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import com.jakewharton.processphoenix.ProcessPhoenix
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.sopt.common.intentprovider.IntentProvider
+import org.sopt.datastore.datastore.SecurityDataStore
 import org.sopt.mainfeature.R
 import org.sopt.mypage.databinding.FragmentSettingsBinding
+import org.sopt.ui.fragment.viewLifeCycle
+import org.sopt.ui.fragment.viewLifeCycleScope
+import org.sopt.ui.view.UiState
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment() {
-
   private lateinit var binding: FragmentSettingsBinding
+  private val viewModel: SettingsViewModel by viewModels()
 
+  @Inject
+  lateinit var intentProvider: IntentProvider
+
+  @Inject
+  lateinit var dataStore: SecurityDataStore
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
@@ -34,5 +54,39 @@ class SettingsFragment : Fragment() {
       tvSettingsAlertOff.visibility =
         if (toasterToggle.getState() == R.id.start) View.VISIBLE else View.INVISIBLE
     }
+
+    binding.tvSetLogout.setOnClickListener {
+      viewModel.logout()
+    }
+
+    viewModel.logoutState.flowWithLifecycle(viewLifeCycle).onEach { state ->
+      when (state) {
+        is UiState.Success -> {
+          dataStore.setAutoLogin(false)
+          val intent = intentProvider.getAuthIntent()
+          startActivity(intent)
+          requireActivity().finish()
+        }
+
+        else -> {}
+      }
+    }.launchIn(viewLifeCycleScope)
+
+    binding.tvWithdraw.setOnClickListener {
+      viewModel.withdraw()
+    }
+
+    viewModel.withdrawState.flowWithLifecycle(viewLifeCycle).onEach { state ->
+      when (state) {
+        is UiState.Success -> {
+          dataStore.setAutoLogin(false)
+          val intent = intentProvider.getAuthIntent()
+          startActivity(intent)
+          requireActivity().finish()
+        }
+        else -> {}
+      }
+    }.launchIn(viewLifeCycleScope)
+
   }
 }
