70780b618aa4bb7155dd4f127e8f414a8b13939f
Sangwook123 <operawook@catholic.ac.kr>
2024-01-07 10:49:25 +0900

    [feat] #18 timer clip select fragment

--- feature/timer/src/main/java/org/sopt/timer/settimer/TimerClipSelectFragment.kt
+++ feature/timer/src/main/java/org/sopt/timer/settimer/TimerClipSelectFragment.kt
@@ -1,19 +1,44 @@
 package org.sopt.timer.settimer
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import designsystem.components.button.state.LinkMindButtonState
 import org.sopt.timer.R
 import org.sopt.timer.databinding.FragmentTimerClipSelectBinding
+import org.sopt.timer.dummymodel.Clip
 import org.sopt.ui.base.BindingFragment
 
-class TimerClipSelectFragment : BindingFragment<FragmentTimerClipSelectBinding>({ FragmentTimerClipSelectBinding.inflate(it)}){
+class TimerClipSelectFragment : BindingFragment<FragmentTimerClipSelectBinding>({ FragmentTimerClipSelectBinding.inflate(it) }) {
+  lateinit var adapter: ClipSelectAdapter
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
     binding.btnTimerClipSelectNext.state = LinkMindButtonState.DISABLE
+
+    var list = listOf(
+      Clip("전체 클립", 3, false),
+      Clip("전체 클립", 3, false),
+      Clip("전체 클립", 3, false),
+      Clip("전체 클립", 3, false),
+    )
+    adapter = ClipSelectAdapter({ a, b ->
+      if (a.isSelected) {
+        list.onEach { it.isSelected = false }
+        list[b].isSelected = true
+        Log.e("리스트", "$list")
+        binding.btnTimerClipSelectNext.state = LinkMindButtonState.ENABLE
+      } else {
+        list.onEach { it.isSelected = false }
+        Log.e("리스트", "$list")
+        binding.btnTimerClipSelectNext.state = LinkMindButtonState.DISABLE
+      }
+    }, requireContext(),)
+    adapter.submitList(list)
+    binding.rvItemTimerClipSelect.adapter = adapter
+    binding.btnTimerClipSelectNext.btnClick {
+      findNavController().navigate(R.id.action_navigation_timer_clip_select_to_navigation_time_picker)
+    }
   }
 }
