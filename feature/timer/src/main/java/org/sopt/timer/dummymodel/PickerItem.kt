20616d45175c6691088b7d3968a3360b84e129ee
Sangwook123 <operawook@catholic.ac.kr>
2024-01-07 10:59:38 +0900

    [feat] #18 텍스트 변환

--- feature/timer/src/main/java/org/sopt/timer/dummymodel/PickerItem.kt
+++ feature/timer/src/main/java/org/sopt/timer/dummymodel/PickerItem.kt
@@ -3,4 +3,8 @@ package org.sopt.timer.dummymodel
 data class PickerItem(
   val text: String,
   val isSelected: Boolean,
-)
+){
+  fun convertToText() = this.text.toIntOrNull()?.let {
+    if (it < 10) "0${this.text}" else this.text
+  } ?: this.text
+}
