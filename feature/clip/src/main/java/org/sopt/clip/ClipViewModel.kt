f02fd205c9272bfa2cd4255dc65248fcca5cc58e
Choi Min Young <codingmy@naver.com>
2024-01-10 03:01:39 +0900

    [add] #73 mock data 추가

--- feature/clip/src/main/java/org/sopt/clip/ClipViewModel.kt
+++ feature/clip/src/main/java/org/sopt/clip/ClipViewModel.kt
@@ -7,6 +7,15 @@ class ClipViewModel : ViewModel() {
     ClipsDTO("a", 1, 1),
     ClipsDTO("b", 2, 2),
     ClipsDTO("c", 3, 3),
+    ClipsDTO("a", 1, 1),
+    ClipsDTO("b", 2, 2),
+    ClipsDTO("c", 3, 3),
+    ClipsDTO("a", 1, 1),
+    ClipsDTO("b", 2, 2),
+    ClipsDTO("c", 3, 3),
+    ClipsDTO("a", 1, 1),
+    ClipsDTO("b", 2, 2),
+    ClipsDTO("c", 3, 3),
     )
 
   val mockLinkData = listOf<LinkDTO>(
@@ -14,6 +23,13 @@ class ClipViewModel : ViewModel() {
     LinkDTO("www.i.com", 12, "제목2", "맛집2"),
     LinkDTO("www.n.com", 12, "제목3", "맛집3"),
     LinkDTO("www.k.com", 12, "제목4", "맛집4"),
-
+    LinkDTO("www.l.com", 12, "제목1", "맛집"),
+    LinkDTO("www.i.com", 12, "제목2", "맛집2"),
+    LinkDTO("www.n.com", 12, "제목3", "맛집3"),
+    LinkDTO("www.k.com", 12, "제목4", "맛집4"),
+    LinkDTO("www.l.com", 12, "제목1", "맛집"),
+    LinkDTO("www.i.com", 12, "제목2", "맛집2"),
+    LinkDTO("www.n.com", 12, "제목3", "맛집3"),
+    LinkDTO("www.k.com", 12, "제목4", "맛집4"),
     )
 }
