f85cefe78f5f857a47fa043f25fddc5a0044a1fc
Choi Min Young <codingmy@naver.com>
2024-01-10 04:23:30 +0900

    [feat] #73 토글 클릭 ui이벤트 구현

--- feature/clip/src/main/java/org/sopt/clip/clipdetail/ClipDetailFragment.kt
+++ feature/clip/src/main/java/org/sopt/clip/clipdetail/ClipDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import org.sopt.clip.ClipViewModel
+import org.sopt.clip.R
 import org.sopt.clip.databinding.FragmentClipDetailBinding
 import org.sopt.ui.base.BindingFragment
 
@@ -17,5 +18,34 @@ class ClipDetailFragment : BindingFragment<FragmentClipDetailBinding>({ Fragment
     val clipDetailAdapter = ClipLinkAdapter({})
     binding.rvCategoryLink.adapter = clipDetailAdapter
     clipDetailAdapter.submitList(viewModel.mockLinkData)
+
+    toggleClickListener()
+  }
+
+  fun toggleClickListener() {
+    with(binding) {
+      btnClipAll.setOnClickListener {
+        setButtonTransparent()
+        btnClipAll.setBackgroundResource(R.drawable.shape_grey050_fill_12_rect)
+      }
+
+      btnClipRead.setOnClickListener {
+        setButtonTransparent()
+        btnClipRead.setBackgroundResource(R.drawable.shape_grey050_fill_12_rect)
+      }
+
+      btnClipUnread.setOnClickListener {
+        setButtonTransparent()
+        btnClipUnread.setBackgroundResource(R.drawable.shape_grey050_fill_12_rect)
+      }
+    }
+  }
+
+  fun setButtonTransparent() {
+    with(binding) {
+      btnClipAll.setBackgroundResource(org.sopt.mainfeature.R.color.transparent)
+      btnClipRead.setBackgroundResource(org.sopt.mainfeature.R.color.transparent)
+      btnClipUnread.setBackgroundResource(org.sopt.mainfeature.R.color.transparent)
+    }
   }
 }
