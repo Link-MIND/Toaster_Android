a8b64a6237560d5759d11140a65ef9180a7907a8
Sangwook123 <operawook@catholic.ac.kr>
2024-01-10 08:16:29 +0900

    [refactor] #68 클립 선택 프래그먼트

--- feature/timer/src/main/java/org/sopt/timer/settimer/clipselect/TimerClipSelectFragment.kt
+++ feature/timer/src/main/java/org/sopt/timer/settimer/clipselect/TimerClipSelectFragment.kt
@@ -1,47 +1,68 @@
 package org.sopt.timer.settimer.clipselect
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import designsystem.components.button.state.LinkMindButtonState
 import org.sopt.timer.R
 import org.sopt.timer.databinding.FragmentTimerClipSelectBinding
 import org.sopt.timer.dummymodel.Clip
+import org.sopt.timer.settimer.SetTimerViewModel
 import org.sopt.ui.base.BindingFragment
+import org.sopt.ui.view.onThrottleClick
 
 class TimerClipSelectFragment : BindingFragment<FragmentTimerClipSelectBinding>({ FragmentTimerClipSelectBinding.inflate(it) }) {
   private lateinit var adapter: ClipSelectAdapter
+
+  private val viewModel: SetTimerViewModel by activityViewModels()
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    binding.btnTimerClipSelectNext.state = LinkMindButtonState.DISABLE
+    val list = viewModel.clipList.value
+    initClipSelectAdapter(list)
+    initNextButtonClickListener(list)
+    initCloseButtonClickListener()
+  }
 
-    var list = listOf(
-      Clip("전체 클립", 3, false),
-      Clip("전체 클립", 3, false),
-      Clip("전체 클립", 3, false),
-      Clip("전체 클립", 3, false),
-    )
+  private fun initCloseButtonClickListener() {
+    binding.ivTimerClipSelectClose.onThrottleClick {
+      findNavController().navigateUp()
+    }
+  }
+
+  private fun initNextButtonClickListener(list: List<Clip>) {
+    binding.btnTimerClipSelectNext.btnClick {
+      viewModel.setClipList(list)
+      findNavController().navigate(R.id.action_navigation_timer_clip_select_to_navigation_time_picker)
+    }
+  }
+
+  private fun initClipSelectAdapter(list: List<Clip>) {
     adapter = ClipSelectAdapter(
-      onClick = { a, b ->
-        if (a.isSelected) {
-          list.onEach { it.isSelected = false }
-          list[b].isSelected = true
-          Log.e("리스트", "$list")
-          binding.btnTimerClipSelectNext.state = LinkMindButtonState.ENABLE
-        } else {
-          list.onEach { it.isSelected = false }
-          Log.e("리스트", "$list")
-          binding.btnTimerClipSelectNext.state = LinkMindButtonState.DISABLE
-        }
+      onClick = { clip, index ->
+        handleClipClick(clip, list, index)
       },
       context = requireContext(),
     )
+    adapter.selectedPosition = list.indexOfFirst { it.isSelected }
     adapter.submitList(list)
+    binding.btnTimerClipSelectNext.state = if (adapter.selectedPosition != -1) LinkMindButtonState.ENABLE else LinkMindButtonState.DISABLE
     binding.rvItemTimerClipSelect.adapter = adapter
-    binding.btnTimerClipSelectNext.btnClick {
-      findNavController().navigate(R.id.action_navigation_timer_clip_select_to_navigation_time_picker)
+  }
+
+  private fun handleClipClick(
+    clip: Clip,
+    list: List<Clip>,
+    index: Int,
+  ) {
+    if (clip.isSelected) {
+      list.onEach { it.isSelected = false }
+      list[index].isSelected = true
+      binding.btnTimerClipSelectNext.state = LinkMindButtonState.ENABLE
+    } else {
+      list.onEach { it.isSelected = false }
+      binding.btnTimerClipSelectNext.state = LinkMindButtonState.DISABLE
     }
   }
 }
