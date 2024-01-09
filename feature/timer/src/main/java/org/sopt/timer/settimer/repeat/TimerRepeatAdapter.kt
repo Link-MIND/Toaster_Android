62ba3f0c0103910cca25d32d77d1c99f1169835b
Sangwook123 <operawook@catholic.ac.kr>
2024-01-09 17:10:21 +0900

    [feat] #18 timer repeat adapter

---
+++ feature/timer/src/main/java/org/sopt/timer/settimer/repeat/TimerRepeatAdapter.kt
@@ -0,0 +1,32 @@
+package org.sopt.timer.settimer.repeat
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import org.sopt.timer.databinding.ItemTimerRepeatBinding
+import org.sopt.timer.dummymodel.Repeat
+import org.sopt.ui.view.ItemDiffCallback
+
+class TimerRepeatAdapter(
+  private val onClick: (Repeat, Int) -> Unit,
+  private val context: Context
+) : ListAdapter<Repeat, TimerRepeatViewHolder>(DiffUtil) {
+  override fun onBindViewHolder(holder: TimerRepeatViewHolder, position: Int) {
+    holder.onBind(getItem(position), onClick)
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimerRepeatViewHolder {
+    return TimerRepeatViewHolder(
+      ItemTimerRepeatBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+      context
+    )
+  }
+
+  companion object {
+    private val DiffUtil = ItemDiffCallback<Repeat>(
+      onItemsTheSame = { old, new -> old == new },
+      onContentsTheSame = { old, new -> old == new },
+    )
+  }
+}
