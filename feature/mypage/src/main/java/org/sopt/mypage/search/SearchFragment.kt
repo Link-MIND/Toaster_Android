29a752e0615553f7e9cb59be749c73e9a8fbaa79
CHANGEL <107169027+CHANGEL1004@users.noreply.github.com>
2024-01-10 01:53:42 +0900

    [feat] : #60 검색 기능 구현 수정

--- feature/mypage/src/main/java/org/sopt/mypage/search/SearchFragment.kt
+++ feature/mypage/src/main/java/org/sopt/mypage/search/SearchFragment.kt
@@ -1,35 +1,27 @@
 package org.sopt.mypage.search
 
-import android.content.Context
 import android.graphics.Typeface
 import android.os.Bundle
-import android.text.Editable
 import android.text.Spannable
 import android.text.SpannableString
-import android.text.TextWatcher
 import android.text.style.StyleSpan
 import android.view.View
-import android.view.inputmethod.InputMethodManager
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.ConcatAdapter
 import org.sopt.mypage.databinding.FragmentSearchBinding
 import org.sopt.mypage.search.adapter.ClipResultAdapter
 import org.sopt.mypage.search.adapter.LinkResultAdapter
-import org.sopt.mypage.search.viewholder.ClipResultViewHolder
-import org.sopt.mypage.search.viewholder.LinkResultViewHolder
 import org.sopt.ui.base.BindingFragment
+import org.sopt.ui.view.onThrottleClick
 
 class SearchFragment : BindingFragment<FragmentSearchBinding>({ FragmentSearchBinding.inflate(it) }) {
 
+  private val viewModel: SearchViewModel by viewModels()
   private lateinit var linkResultAdapter: LinkResultAdapter
   private lateinit var clipResultAdapter: ClipResultAdapter
   private lateinit var mResultAdapter: ConcatAdapter
-
-
-  private val linkResults = listOf(
-    LinkResultDummy("카테고리", "Title", "URL"),
-    LinkResultDummy("카테고리2", "Title2", "URL2"),
-  )
-  private val clipResults = listOf(ClipResultDummy("토스터기", 6), ClipResultDummy("토스터", 8))
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
@@ -37,90 +29,63 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>({ FragmentSearchBi
     linkResultAdapter = LinkResultAdapter()
     clipResultAdapter = ClipResultAdapter()
 
-    linkResultAdapter.submitList(linkResults)
-    clipResultAdapter.submitList(clipResults)
+    viewModel.linkResults.observe(
+      viewLifecycleOwner,
+      Observer {
+        linkResultAdapter.submitList(it)
+      },
+    )
+
+    viewModel.clipResults.observe(
+      viewLifecycleOwner,
+      Observer {
+        clipResultAdapter.submitList(it)
+      },
+    )
+
+    viewModel.matchingTitles.observe(
+      viewLifecycleOwner,
+      Observer {
+        handleMatchingTitlesUpdate(it)
+      },
+    )
 
     mResultAdapter = ConcatAdapter(linkResultAdapter, clipResultAdapter)
     binding.rcSearchResult.adapter = mResultAdapter
 
-    binding.ivLeft.setOnClickListener {
+    binding.ivLeft.onThrottleClick {
       requireActivity().supportFragmentManager.popBackStack()
     }
 
-    binding.ivCancel.setOnClickListener {
+    binding.ivCancel.onThrottleClick {
       binding.editText.text.clear()
-
-      val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-      imm.hideSoftInputFromWindow(binding.editText.windowToken, 0)
     }
 
-    binding.ivSearch.setOnClickListener {
+    binding.ivSearch.onThrottleClick {
       val searchTerm = binding.editText.text.toString()
-      performSearch(searchTerm)
+      viewModel.searchResults(searchTerm)
     }
 
-    binding.editText.addTextChangedListener(
-      object : TextWatcher {
-        override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
-          binding.ivCancel.visibility = if (charSequence.isNullOrEmpty()) View.INVISIBLE else View.VISIBLE
-        }
-
-        override fun afterTextChanged(editable: Editable?) {}
-      },
-    )
-
-    binding.ivCancel.setOnClickListener {
-      binding.editText.text.clear()
-      hideSoftKeyboard()
+    binding.editText.doAfterTextChanged { text ->
+      val isTextEmpty = text.isNullOrEmpty()
+      binding.ivCancel.visibility = if (isTextEmpty) View.INVISIBLE else View.VISIBLE
     }
+
   }
 
-  private fun performSearch(searchTerm: String) {
-
-    val matchingResults = findMatchingResults(searchTerm)
-
-    if (matchingResults.matchingTitles.isEmpty()) {
-
+  private fun handleMatchingTitlesUpdate(matchingTitles: List<String>) {
+    if (matchingTitles.isEmpty()) {
       binding.clNoneResults.visibility = View.VISIBLE
       binding.rcSearchResult.visibility = View.GONE
     } else {
-
       binding.clNoneResults.visibility = View.GONE
       binding.rcSearchResult.visibility = View.VISIBLE
-
-      linkResultAdapter.submitList(matchingResults.linkResults)
-      clipResultAdapter.submitList(matchingResults.clipResults)
-
-      applyBoldStyling(matchingResults.matchingTitles, searchTerm)
+      mResultAdapter.notifyDataSetChanged()
+      applyTextToBold(matchingTitles, viewModel.searchTerm)
     }
   }
 
-  private fun findMatchingResults(searchTerm: String): MatchingResults {
-    val matchingTitles = mutableListOf<String>()
-    val matchingLinkResults = mutableListOf<LinkResultDummy>()
-    val matchingClipResults = mutableListOf<ClipResultDummy>()
-
-
-    for (linkResult in linkResults) {
-      if (linkResult.title.contains(searchTerm, ignoreCase = true)) {
-        matchingTitles.add(linkResult.title)
-        matchingLinkResults.add(linkResult)
-      }
-    }
-
-    for (clipResult in clipResults) {
-      if (clipResult.title.contains(searchTerm, ignoreCase = true)) {
-        matchingTitles.add(clipResult.title)
-        matchingClipResults.add(clipResult)
-      }
-    }
-
-    return MatchingResults(matchingLinkResults, matchingClipResults, matchingTitles)
-  }
-
-  private fun applyBoldStyling(matchingTitles: List<String>, searchTerm: String) {
+  private fun applyTextToBold(matchingTitles: List<String>, searchTerm: String) {
     for (matchingTitle in matchingTitles) {
       val start = matchingTitle.indexOf(searchTerm, ignoreCase = true)
       val end = start + searchTerm.length
@@ -133,41 +98,8 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>({ FragmentSearchBi
           end,
           Spannable.SPAN_INCLUSIVE_INCLUSIVE,
         )
-
-
-        val adapterPosition = findAdapterPosition(matchingTitle)
-        if (adapterPosition != -1) {
-          val viewHolder =
-            binding.rcSearchResult.findViewHolderForAdapterPosition(adapterPosition)
-          if (viewHolder is LinkResultViewHolder) {
-            viewHolder.binding.tvClipTitle.text = spannableString
-          } else if (viewHolder is ClipResultViewHolder) {
-            viewHolder.binding.tvClipTitle.text = spannableString
-          }
-        }
       }
     }
-  }
-
-  private fun findAdapterPosition(matchingTitle: String): Int {
-    for ((index, item) in linkResultAdapter.currentList.withIndex()) {
-      if (item is LinkResultDummy && item.title == matchingTitle) {
-        return index
-      }
-    }
-    for ((index, item) in clipResultAdapter.currentList.withIndex()) {
-      if (item is ClipResultDummy && item.title == matchingTitle) {
-        return index + linkResultAdapter.itemCount
-      }
-    }
-
-    return -1
-  }
-
-  private fun hideSoftKeyboard() {
-    val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(binding.editText.windowToken, 0)
   }
 }
-
 
