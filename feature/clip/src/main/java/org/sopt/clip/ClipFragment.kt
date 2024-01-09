e30dc11dced141cb84a665a0af99ddc43193d37c
Choi Min Young <codingmy@naver.com>
2024-01-09 23:01:23 +0900

    [feat] clip뷰 리스트뷰 구현

--- feature/clip/src/main/java/org/sopt/clip/ClipFragment.kt
+++ feature/clip/src/main/java/org/sopt/clip/ClipFragment.kt
@@ -1,7 +1,6 @@
 package org.sopt.clip
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import org.sopt.clip.databinding.FragmentClipBinding
@@ -13,32 +12,12 @@ class ClipFragment : BindingFragment<FragmentClipBinding>({ FragmentClipBinding.
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?,
-  ): View? {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_clip, container, false)
-  }
+    val clipAdapter = ClipAdapter(
+      {},
+    )
+    binding.rvClipClip.adapter = clipAdapter
 
-  companion object {
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ClipFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    @JvmStatic
-    fun newInstance(param1: String, param2: String) =
-      ClipFragment().apply {
-        arguments = Bundle().apply {
-          putString(ARG_PARAM1, param1)
-          putString(ARG_PARAM2, param2)
-        }
-      }
+    clipAdapter.submitList(viewModel.mockClipData)
+
   }
 }
