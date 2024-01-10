23987cafecdb9648c24fe0b3297154eb6dd8b67b
Choi Min Young <codingmy@naver.com>
2024-01-10 15:01:07 +0900

    [feat] #73 토글 클릭이벤트 구현

--- feature/clip/src/main/java/org/sopt/clip/clipdetail/ClipDetailFragment.kt
+++ feature/clip/src/main/java/org/sopt/clip/clipdetail/ClipDetailFragment.kt
@@ -2,8 +2,11 @@ package org.sopt.clip.clipdetail
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import org.sopt.clip.ClipViewModel
+import org.sopt.clip.LinkDTO
 import org.sopt.clip.R
 import org.sopt.clip.databinding.FragmentClipDetailBinding
 import org.sopt.ui.base.BindingFragment
@@ -16,35 +19,70 @@ class ClipDetailFragment : BindingFragment<FragmentClipDetailBinding>({ Fragment
 
     val clipDetailAdapter = ClipLinkAdapter()
     binding.rvCategoryLink.adapter = clipDetailAdapter
-    clipDetailAdapter.submitList(viewModel.mockLinkData)
+    var state: Boolean = viewModel.mockLinkData == null
+    initEmptyMsgVisible(state)
+    if (!state) {
+      clipDetailAdapter.submitList(viewModel.mockLinkData)
+    }
 
     toggleClickListener()
   }
 
-  fun toggleClickListener() {
+  fun toggleClickListener() : List<LinkDTO> {
     with(binding) {
       btnClipAll.setOnClickListener {
-        setButtonTransparent()
-        btnClipAll.setBackgroundResource(R.drawable.shape_grey050_fill_12_rect)
+        initButtonTransparent()
+        initTextGrey()
+        dvClipPicker1.isVisible = false
+        dvClipPicker2.isVisible = true
+        btnClipAll.setBackgroundResource(R.drawable.shape_white_fill_12_rect)
+        btnClipAll.setTextAppearance(org.sopt.mainfeature.R.style.Typography_suit_bold_14)
       }
 
       btnClipRead.setOnClickListener {
-        setButtonTransparent()
-        btnClipRead.setBackgroundResource(R.drawable.shape_grey050_fill_12_rect)
+        initButtonTransparent()
+        initTextGrey()
+        dvClipPicker1.isVisible = false
+        dvClipPicker2.isVisible = false
+        btnClipRead.setBackgroundResource(R.drawable.shape_white_fill_12_rect)
+        btnClipRead.setTextAppearance(org.sopt.mainfeature.R.style.Typography_suit_bold_14)
       }
 
       btnClipUnread.setOnClickListener {
-        setButtonTransparent()
-        btnClipUnread.setBackgroundResource(R.drawable.shape_grey050_fill_12_rect)
+        initButtonTransparent()
+        initTextGrey()
+        dvClipPicker1.isVisible = true
+        dvClipPicker2.isVisible = false
+        btnClipUnread.setBackgroundResource(R.drawable.shape_white_fill_12_rect)
+        btnClipUnread.setTextAppearance(org.sopt.mainfeature.R.style.Typography_suit_bold_14)
       }
     }
+    return viewModel.mockLinkData
   }
 
-  fun setButtonTransparent() {
+  fun initButtonTransparent() {
     with(binding) {
       btnClipAll.setBackgroundResource(org.sopt.mainfeature.R.color.transparent)
       btnClipRead.setBackgroundResource(org.sopt.mainfeature.R.color.transparent)
       btnClipUnread.setBackgroundResource(org.sopt.mainfeature.R.color.transparent)
+    }
+  }
+
+  fun initEmptyMsgVisible(state: Boolean) {
+    with(binding) {
+        ivClipCategoryEmpty.isVisible = state
+        tvClipDetailEmpty.isVisible = state
+    }
+  }
+
+  fun initTextGrey() {
+    with(binding) {
+      btnClipAll.setTextAppearance(org.sopt.mainfeature.R.style.Typography_suit_semibold_14)
+      btnClipRead.setTextAppearance(org.sopt.mainfeature.R.style.Typography_suit_semibold_14)
+      btnClipUnread.setTextAppearance(org.sopt.mainfeature.R.style.Typography_suit_semibold_14)
+      btnClipAll.setTextColor(ContextCompat.getColor(root.context, org.sopt.mainfeature.R.color.neutrals400 ))
+      btnClipRead.setTextColor(ContextCompat.getColor(root.context, org.sopt.mainfeature.R.color.neutrals400 ))
+      btnClipUnread.setTextColor(ContextCompat.getColor(root.context, org.sopt.mainfeature.R.color.neutrals400 ))
     }
   }
 }
