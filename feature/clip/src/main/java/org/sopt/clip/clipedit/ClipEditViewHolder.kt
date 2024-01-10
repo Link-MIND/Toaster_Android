93a3f24f8392a493ad587b90579591bc1a1860d6
Choi Min Young <codingmy@naver.com>
2024-01-10 22:36:58 +0900

    [add] #76 리스트뷰 뷰홀더 추가

---
+++ feature/clip/src/main/java/org/sopt/clip/clipedit/ClipEditViewHolder.kt
@@ -0,0 +1,25 @@
+package org.sopt.clip.clipedit
+
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.clip.ClipsDTO
+import org.sopt.clip.databinding.ItemClipEditClipBinding
+
+class ClipEditViewHolder(
+  private val binding: ItemClipEditClipBinding,
+  private val onClickItemClip: (ClipsDTO) -> Unit,
+) : RecyclerView.ViewHolder(binding.root) {
+  fun onBind(clipData: ClipsDTO?) {
+    if (clipData == null) {
+    } else {
+      with(binding) {
+        tvClipEditTitle.text = clipData.clipName
+        clClipEditClip.setOnClickListener {
+          //id 전달
+        }
+        ivClipEditDelete.setOnClickListener {
+          //id 전달
+        }
+      }
+    }
+  }
+}
