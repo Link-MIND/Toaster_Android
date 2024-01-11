f84330376ee95b6c788e44e9e2f4cfa90cfb4720
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-11 23:15:29 +0900

    [feat] : #71 data remote user / category build

--- data-remote/category/build.gradle.kts
+++ data-remote/category/build.gradle.kts
@@ -1,44 +1,15 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.linkmind.plugin.data)
+  alias(libs.plugins.kotlin.serialization)
 }
 
 android {
-    namespace = "org.sopt.category"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+  namespace = "org.sopt.category"
 }
 
 dependencies {
-
-    implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+  implementation(projects.core.network)
+  implementation(projects.data.category)
+  implementation(libs.retrofit.kotlin.serialization)
 }
