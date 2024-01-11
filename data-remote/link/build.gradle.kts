538496da722fe65f5401d4bbdc29f04251852821
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-12 07:13:36 +0900

    [feat] : #92 build set

--- data-remote/link/build.gradle.kts
+++ data-remote/link/build.gradle.kts
@@ -1,44 +1,15 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.linkmind.plugin.data)
+  alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "org.sopt.remote.link"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-
-    implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+  implementation(projects.core.network)
+  implementation(projects.data.link)
+  implementation(libs.retrofit.kotlin.serialization)
 }
--- feature/savelink/build.gradle.kts
+++ feature/savelink/build.gradle.kts
@@ -10,4 +10,5 @@ android {
 
 dependencies {
   implementation(projects.core.designsystem)
+  implementation(projects.domain.link)
 }
