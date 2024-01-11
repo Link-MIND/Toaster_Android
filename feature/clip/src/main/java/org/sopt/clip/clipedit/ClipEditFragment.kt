d96082a128b393a98d05adfdb4712dc422165fd2
Choi Min Young <codingmy@naver.com>
2024-01-11 20:55:09 +0900

    [refactor] #76 함수화

--- feature/clip/src/main/java/org/sopt/clip/clipedit/ClipEditFragment.kt
+++ feature/clip/src/main/java/org/sopt/clip/clipedit/ClipEditFragment.kt
@@ -12,25 +12,32 @@ import org.sopt.ui.view.onThrottleClick
 
 class ClipEditFragment : BindingFragment<FragmentClipEditBinding>({ FragmentClipEditBinding.inflate(it) }) {
   private val viewModel by viewModels<ClipViewModel>()
+  private lateinit var clipEditAdapter: ClipEditAdapter
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    val clipEditAdapter = ClipEditAdapter()
-    binding.rvClipEdit.adapter = clipEditAdapter
-    var state: Boolean = viewModel.mockClipData == null
+    initEditAdapter()
+    updateEditListView()
 
+    onClickBackButton()
+  }
+
+  private fun updateEditListView() {
+    var state: Boolean = viewModel.mockClipData == null
     if (!state) {
       clipEditAdapter.submitList(viewModel.mockClipData)
     }
+  }
 
-    with(binding) {
-      ivClipEditBack.setOnClickListener {
-      }
-    }
+  private fun initEditAdapter() {
+    val clipEditAdapter = ClipEditAdapter()
+    binding.rvClipEdit.adapter = clipEditAdapter
+  }
 
+  private fun onClickBackButton() {
     binding.ivClipEditBack.onThrottleClick {
-      findNavController().navigate(R.id.action_navigation_clip_edit_to_navigation_clip)
+      findNavController().navigateUp()
     }
   }
 }
