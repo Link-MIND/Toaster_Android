ded40eaaabdcafb194f816f16e191a9d805f5a5e
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-12 07:15:28 +0900

    [feat] : #92 di set

--- data-remote/category/src/main/java/org/sopt/dataremote/category/di/CategoryServiceModule.kt
+++ data-remote/category/src/main/java/org/sopt/dataremote/category/di/CategoryServiceModule.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object ServiceModule {
+object CategoryServiceModule {
 
   @Singleton
   @Provides
---
+++ data-remote/link/src/main/java/org/sopt/remote/link/di/LinkDataSourceModule.kt
@@ -0,0 +1,20 @@
+package org.sopt.remote.link.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.data.link.datasource.RemoteLinkDataSource
+import org.sopt.remote.link.datasource.RemoteLinkDataSourceImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LinkDataSourceModule {
+  @Singleton
+  @Binds
+  abstract fun bindRemoteLectureProviderDatasource(
+    remoteLinkDataSourceImpl: RemoteLinkDataSourceImpl,
+  ): RemoteLinkDataSource
+
+}
