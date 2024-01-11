63c95423c0da570ce36b6da7780e3ecec53407fc
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-12 01:59:46 +0900

    [feat] : #85 remote get categories datasource

---
+++ data/category/src/main/java/org/sopt/data/category/datasource/RemoteCategoryDataSource.kt
@@ -0,0 +1,7 @@
+package org.sopt.data.category.datasource
+
+import org.sopt.model.sample.category.CategoryList
+
+interface RemoteCategoryDataSource {
+  suspend fun getCategoryAll():List<CategoryList>
+}
