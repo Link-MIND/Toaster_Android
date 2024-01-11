b5be81ee36c24578f5f979a794a07f3c6dba5489
Sangwook123 <operawook@catholic.ac.kr>
2024-01-12 03:11:08 +0900

    [feat] #87 getTimer data source

--- data/timer/src/main/java/org/sopt/timer/source/remote/TimerRemoteDataSource.kt
+++ data/timer/src/main/java/org/sopt/timer/source/remote/TimerRemoteDataSource.kt
@@ -1,5 +1,8 @@
 package org.sopt.timer.source.remote
 
+import org.sopt.network.model.response.base.BaseResponse
+import org.sopt.timer.model.remote.ResponseGetTimerPageDto
+
 interface TimerRemoteDataSource {
-  //TODO
+  suspend fun getTimerMain(): BaseResponse<ResponseGetTimerPageDto>
 }
