e9c32fb91aaa03b4d517782762c9aaffffe6a1ef
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-12 01:59:46 +0900

    [feat] : #85 remote get categories datasource

---
+++ data-remote/category/src/main/java/org/sopt/dataremote/category/datasource/RemoteCategoryDataSourceImpl.kt
@@ -0,0 +1,16 @@
+package org.sopt.dataremote.category.datasource
+
+import org.sopt.data.category.datasource.RemoteCategoryDataSource
+import org.sopt.dataremote.category.api.CategoryService
+import org.sopt.dataremote.category.response.toCoreModel
+import org.sopt.model.sample.category.CategoryList
+import javax.inject.Inject
+
+class RemoteCategoryDataSourceImpl @Inject constructor(
+  private val categoryService: CategoryService
+): RemoteCategoryDataSource {
+  override suspend fun getCategoryAll(): List<CategoryList> =
+    categoryService.getCategoryAll().data!!.map {
+      it.toCoreModel()
+    }
+}
