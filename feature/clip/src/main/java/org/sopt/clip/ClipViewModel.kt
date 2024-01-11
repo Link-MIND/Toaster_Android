167b041f832478458c0c4578ede3d6ffee35e6d2
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-12 02:00:12 +0900

    [feat] : #85 usecase 적용

--- feature/clip/src/main/java/org/sopt/clip/ClipViewModel.kt
+++ feature/clip/src/main/java/org/sopt/clip/ClipViewModel.kt
@@ -1,8 +1,29 @@
 package org.sopt.clip
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import org.sopt.domain.category.category.repository.CategoryRepository
+import org.sopt.domain.category.category.usecase.GetCategoryAllUseCase
+import javax.inject.Inject
 
-class ClipViewModel : ViewModel() {
+@HiltViewModel
+class ClipViewModel @Inject constructor(
+  private val getCategoryAll: GetCategoryAllUseCase
+) : ViewModel() {
+  init {
+      getCategoryAll()
+  }
+  fun getCategoryAll() = viewModelScope.launch {
+    getCategoryAll.invoke().onSuccess {
+      Log.d("test","$it")
+    }.onFailure {
+
+    }
+  }
+
   val mockClipData = listOf<ClipsDTO>(
     ClipsDTO("a", 1, 1),
     ClipsDTO("b", 2, 2),
@@ -32,4 +53,5 @@ class ClipViewModel : ViewModel() {
     LinkDTO("www.n.com", 12, "제목3", "맛집3"),
     LinkDTO("www.k.com", 12, "제목4", "맛집4"),
   )
+
 }
