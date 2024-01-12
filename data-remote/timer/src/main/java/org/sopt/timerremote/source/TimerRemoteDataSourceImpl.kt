0ea2e96031e003b1ad461894042a24b6fde33897
Sangwook123 <operawook@catholic.ac.kr>
2024-01-12 17:57:41 +0900

    [feat] #95 timer remote data source

--- data-remote/timer/src/main/java/org/sopt/timerremote/source/TimerRemoteDataSourceImpl.kt
+++ data-remote/timer/src/main/java/org/sopt/timerremote/source/TimerRemoteDataSourceImpl.kt
@@ -1,7 +1,8 @@
 package org.sopt.timerremote.source
 
 import org.sopt.network.model.response.base.BaseResponse
-import org.sopt.timer.model.remote.ResponseGetTimerPageDto
+import org.sopt.timer.model.remote.request.RequestPostTimerDto
+import org.sopt.timer.model.remote.response.ResponseGetTimerPageDto
 import org.sopt.timer.source.remote.TimerRemoteDataSource
 import org.sopt.timerremote.api.TimerService
 import javax.inject.Inject
@@ -10,4 +11,5 @@ class TimerRemoteDataSourceImpl @Inject constructor(
   private val timerService: TimerService,
 ) : TimerRemoteDataSource {
   override suspend fun getTimerMain(): BaseResponse<ResponseGetTimerPageDto> = timerService.getTimerMain()
+  override suspend fun postTimer(requestPostTimerDto: RequestPostTimerDto): BaseResponse<Unit> = timerService.postTimer(requestPostTimerDto)
 }
--- data/timer/src/main/java/org/sopt/timer/source/remote/TimerRemoteDataSource.kt
+++ data/timer/src/main/java/org/sopt/timer/source/remote/TimerRemoteDataSource.kt
@@ -1,8 +1,11 @@
 package org.sopt.timer.source.remote
 
 import org.sopt.network.model.response.base.BaseResponse
-import org.sopt.timer.model.remote.ResponseGetTimerPageDto
+import org.sopt.timer.model.remote.request.RequestPostTimerDto
+import org.sopt.timer.model.remote.response.ResponseGetTimerPageDto
 
 interface TimerRemoteDataSource {
   suspend fun getTimerMain(): BaseResponse<ResponseGetTimerPageDto>
+
+  suspend fun postTimer(requestPostTimerDto: RequestPostTimerDto): BaseResponse<Unit>
 }
