acb7ae4e7aca67271165b629528d3cdcbcb77dc4
Choi Min Young <codingmy@naver.com>
2024-01-13 03:31:05 +0900

    [feat] #98 누락 코드 커밋

--- feature/clip/src/main/java/org/sopt/clip/clip/ClipFragment.kt
+++ feature/clip/src/main/java/org/sopt/clip/clip/ClipFragment.kt
@@ -7,9 +7,11 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.clip.ClipViewModel
+import org.sopt.clip.R
 import org.sopt.clip.databinding.FragmentClipBinding
 import org.sopt.ui.base.BindingFragment
 import org.sopt.ui.view.onThrottleClick
+
 @AndroidEntryPoint
 class ClipFragment : BindingFragment<FragmentClipBinding>({ FragmentClipBinding.inflate(it) }) {
   private val viewModel by viewModels<ClipViewModel>()
