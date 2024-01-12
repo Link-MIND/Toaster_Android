86c38e13f9ee7f93f5f408901d0a53ccd644f9cc
Sangwook123 <operawook@catholic.ac.kr>
2024-01-12 17:57:23 +0900

    [feat] #95 timer service

--- data-remote/timer/src/main/java/org/sopt/timerremote/api/TimerService.kt
+++ data-remote/timer/src/main/java/org/sopt/timerremote/api/TimerService.kt
@@ -1,10 +1,18 @@
 package org.sopt.timerremote.api
 
 import org.sopt.network.model.response.base.BaseResponse
-import org.sopt.timer.model.remote.ResponseGetTimerPageDto
+import org.sopt.timer.model.remote.request.RequestPostTimerDto
+import org.sopt.timer.model.remote.response.ResponseGetTimerPageDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface TimerService {
   @GET("timer/main")
   suspend fun getTimerMain(): BaseResponse<ResponseGetTimerPageDto>
+
+  @POST("timer")
+  suspend fun postTimer(
+    @Body requestPostTimerDto: RequestPostTimerDto
+  ): BaseResponse<Unit>
 }
