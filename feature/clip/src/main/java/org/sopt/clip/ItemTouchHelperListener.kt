73592e1991556a2905d22c0cf675851f963f1563
Choi Min Young <codingmy@naver.com>
2024-01-13 00:05:02 +0900

    [feat] #98 클립 drag&drop 구현

---
+++ feature/clip/src/main/java/org/sopt/clip/ItemTouchHelperListener.kt
@@ -0,0 +1,5 @@
+package org.sopt.clip
+
+interface ItemTouchHelperListener {
+  fun onItemMove(from: Int, to: Int)
+}
--- feature/clip/src/main/java/org/sopt/clip/clipedit/ClipEditFragment.kt
+++ feature/clip/src/main/java/org/sopt/clip/clipedit/ClipEditFragment.kt
@@ -2,8 +2,10 @@ package org.sopt.clip.clipedit
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.clip.ClipViewModel
 import org.sopt.clip.databinding.FragmentClipEditBinding
@@ -14,11 +16,18 @@ import org.sopt.ui.view.onThrottleClick
 class ClipEditFragment : BindingFragment<FragmentClipEditBinding>({ FragmentClipEditBinding.inflate(it) }) {
   private val viewModel by viewModels<ClipViewModel>()
   private lateinit var clipEditAdapter: ClipEditAdapter
+  private val itemTouchHelper by lazy { ItemTouchHelper(ItemTouchCallback(clipEditAdapter))}
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    clipEditAdapter = ClipEditAdapter()
+    clipEditAdapter = ClipEditAdapter{ itemId, state ->
+      val todo:String
+      if(state=="delete")
+        todo="삭제"
+      else todo="수정"
+      Toast.makeText(context, "$todo + itemId: $itemId", Toast.LENGTH_SHORT).show()
+    }
     binding.rvClipEdit.adapter = clipEditAdapter
 
     updateEditListView()
