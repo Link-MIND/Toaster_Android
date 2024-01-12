247497fa1164239290c17e192d7b5f558dcb4f20
Sangwook123 <operawook@catholic.ac.kr>
2024-01-12 18:28:15 +0900

    [feat] #95 post timer repository

--- data/timer/src/main/java/org/sopt/timer/repository/TimerRepositoryImpl.kt
+++ data/timer/src/main/java/org/sopt/timer/repository/TimerRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package org.sopt.timer.repository
 
+import org.sopt.model.timer.TimerData
 import org.sopt.model.timer.Timer
-import org.sopt.timer.model.remote.toCoreModel
+import org.sopt.timer.model.remote.request.toDataModel
+import org.sopt.timer.model.remote.response.toCoreModel
 import org.sopt.timer.source.remote.TimerRemoteDataSource
 import javax.inject.Inject
 
@@ -10,4 +12,7 @@ class TimerRepositoryImpl @Inject constructor(
 ) : TimerRepository {
   override suspend fun getTimerMain(): Result<Pair<List<Timer>, List<Timer>>?> =
     runCatching { timerRemoteDataSource.getTimerMain().data?.toCoreModel() }
+
+  override suspend fun postTimer(timerData: TimerData): Result<Unit> =
+    runCatching { timerRemoteDataSource.postTimer(timerData.toDataModel()) }
 }
--- domain/timer/src/main/java/org/sopt/timer/repository/TimerRepository.kt
+++ domain/timer/src/main/java/org/sopt/timer/repository/TimerRepository.kt
@@ -1,7 +1,10 @@
 package org.sopt.timer.repository
 
+import org.sopt.model.timer.TimerData
 import org.sopt.model.timer.Timer
 
 interface TimerRepository {
   suspend fun getTimerMain(): Result<Pair<List<Timer>, List<Timer>>?>
+
+  suspend fun postTimer(timerData: TimerData): Result<Unit>
 }
