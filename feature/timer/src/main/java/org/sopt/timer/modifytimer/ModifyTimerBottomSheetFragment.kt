0e212232195b8f7897df726e00b7e47a054466da
Sangwook123 <operawook@catholic.ac.kr>
2024-01-14 23:45:32 +0900

    [feat] #95 timer delete

--- feature/timer/src/main/java/org/sopt/timer/modifytimer/ModifyTimerBottomSheetFragment.kt
+++ feature/timer/src/main/java/org/sopt/timer/modifytimer/ModifyTimerBottomSheetFragment.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import android.view.View
 import org.sopt.timer.databinding.FragmentModifyTimerBottomSheetBinding
 import org.sopt.ui.base.BindingBottomSheetDialogFragment
+import org.sopt.ui.view.onThrottleClick
 
 class ModifyTimerBottomSheetFragment :
   BindingBottomSheetDialogFragment<FragmentModifyTimerBottomSheetBinding>({ FragmentModifyTimerBottomSheetBinding.inflate(it) }) {
   var id: Int? = null
+  private var handleDelete: () -> Unit = {}
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
     binding.ivModifyTimerClose.setOnClickListener {
+      dismiss()
+    }
+
+    binding.tvModifyTimerDelete.onThrottleClick {
+      handleDelete.invoke()
       dismiss()
     }
   }
@@ -20,13 +27,18 @@ class ModifyTimerBottomSheetFragment :
     super.onCreate(savedInstanceState)
     id = arguments?.getInt("id")
   }
+
   companion object {
-    fun newInstance(id: Int): ModifyTimerBottomSheetFragment {
+    fun newInstance(
+      id: Int,
+      handleDeleteButton: () -> Unit,
+    ): ModifyTimerBottomSheetFragment {
       val args = Bundle().apply {
         putInt("id", id)
       }
       return ModifyTimerBottomSheetFragment().apply {
         arguments = args
+        handleDelete = handleDeleteButton
       }
     }
   }
