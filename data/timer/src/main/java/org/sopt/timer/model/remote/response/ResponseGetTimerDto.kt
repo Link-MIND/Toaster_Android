b7cb0bd8828e636df081ee60b909147915a8b81a
Sangwook123 <operawook@catholic.ac.kr>
2024-01-14 23:40:10 +0900

    [add] #95 response get timer dto

---
+++ data/timer/src/main/java/org/sopt/timer/model/remote/response/ResponseGetTimerDto.kt
@@ -0,0 +1,14 @@
+package org.sopt.timer.model.remote.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseGetTimerDto(
+  @SerialName("categoryName")
+  val categoryName: String,
+  @SerialName("remindTime")
+  val remindTime: String,
+  @SerialName("remindDates")
+  val remindDates: List<Int>,
+)
