258a1bc9340b860f795983d98f6745d0ceb6c226
CHANGEL <parkchangel@naver.com>
2024-01-14 23:16:15 +0900

    [chore] : #83 코드 정리

--- feature/clip/src/main/java/org/sopt/clip/clipdetail/ClipDetailFragment.kt
+++ feature/clip/src/main/java/org/sopt/clip/clipdetail/ClipDetailFragment.kt
@@ -24,7 +24,6 @@ class ClipDetailFragment : BindingFragment<FragmentClipDetailBinding>({ Fragment
       }
       findNavController().navigate(R.id.action_navigation_clip_detail_to_webViewFragment, bundle)
     }
-
     binding.rvCategoryLink.adapter = clipDetailAdapter
     var state: Boolean = viewModel.mockLinkData == null
     initEmptyMsgVisible(state)
