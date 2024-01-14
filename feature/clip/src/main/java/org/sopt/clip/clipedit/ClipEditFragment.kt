c350d08de682c05acf7823faab44beb13ea7a968
Choi Min Young <codingmy@naver.com>
2024-01-14 22:44:18 +0900

    [feat] #103 edit뷰 클립 삭제 dialog 구현

--- feature/clip/src/main/java/org/sopt/clip/clipedit/ClipEditFragment.kt
+++ feature/clip/src/main/java/org/sopt/clip/clipedit/ClipEditFragment.kt
@@ -8,6 +8,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import dagger.hilt.android.AndroidEntryPoint
 import designsystem.components.bottomsheet.LinkMindBottomSheet
+import designsystem.components.dialog.LinkMindDialog
 import designsystem.components.toast.linkMindSnackBar
 import org.sopt.clip.ClipViewModel
 import org.sopt.clip.databinding.FragmentClipEditBinding
@@ -26,7 +27,9 @@ class ClipEditFragment : BindingFragment<FragmentClipEditBinding>({ FragmentClip
 
     clipEditAdapter = ClipEditAdapter { itemId, state, position ->
       when (state) {
-        "delete" -> {}
+        "delete" -> {
+          showDeleteDialog()
+        }
 
         "edit" -> {
           showHomeBottomSheet()
@@ -75,5 +78,18 @@ class ClipEditFragment : BindingFragment<FragmentClipEditBinding>({ FragmentClip
         requireContext().linkMindSnackBar(binding.root, "클립 수정 완료!", false)
       }
     }
+  }
+
+  private fun showDeleteDialog() {
+    val deleteDialog = LinkMindDialog(requireContext())
+    deleteDialog.setTitle(R.string.edit_clip_delete_dialog_title)
+      .setSubtitle(R.string.edit_clip_delete_dialog_subtitle)
+      .setNegativeButton(R.string.negative_close_msg) {
+        deleteDialog.dismiss()
+      }
+      .setPositiveButton(R.string.edit_clip_delete_dialog_delete) {
+        deleteDialog.dismiss()
+      }
+      .show()
   }
 }
