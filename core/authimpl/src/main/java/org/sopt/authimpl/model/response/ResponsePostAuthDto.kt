7171b2514517e2424b71811df2fc294bad3b7bbf
CHANGEL <parkchangel@naver.com>
2024-01-15 02:14:02 +0900

    [chore] : #102 오타 수정

--- core/authimpl/src/main/java/org/sopt/authimpl/model/response/ResponsePostAuthDto.kt
+++ core/authimpl/src/main/java/org/sopt/authimpl/model/response/ResponsePostAuthDto.kt
@@ -17,7 +17,7 @@ data class ResponsePostAuthDto(
   val fcmToken: String,
   @SerialName("isRegistered")
   val isRegistered: Boolean,
-  @SerialName("FcmIsAllowed")
+  @SerialName("fcmIsAllowed")
   val fcmIsAllowed: Boolean,
   @SerialName("profile")
   val profile: String,
