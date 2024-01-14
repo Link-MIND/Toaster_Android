4cb48270bbcac877621d707944a84868843b59d5
Sangwook123 <operawook@catholic.ac.kr>
2024-01-14 23:39:53 +0900

    [feat] #95 service

--- data-remote/timer/src/main/java/org/sopt/timerremote/api/TimerService.kt
+++ data-remote/timer/src/main/java/org/sopt/timerremote/api/TimerService.kt
@@ -1,11 +1,16 @@
 package org.sopt.timerremote.api
 
 import org.sopt.network.model.response.base.BaseResponse
+import org.sopt.timer.model.remote.request.RequestPatchTimerDto
 import org.sopt.timer.model.remote.request.RequestPostTimerDto
+import org.sopt.timer.model.remote.response.ResponseGetTimerDto
 import org.sopt.timer.model.remote.response.ResponseGetTimerPageDto
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface TimerService {
   @GET("timer/main")
@@ -15,4 +20,20 @@ interface TimerService {
   suspend fun postTimer(
     @Body requestPostTimerDto: RequestPostTimerDto,
   ): BaseResponse<Unit>
+
+  @PATCH("timer/datetime")
+  suspend fun patchTimer(
+    @Path("timerId") timerId: Int,
+    @Body requestPatchTimerDto: RequestPatchTimerDto
+  ): BaseResponse<Unit>
+
+  @DELETE("timer/{timerId}")
+  suspend fun deleteTimer(
+    @Path("timerId") timerId: Int,
+  ): BaseResponse<Unit>
+
+  @GET("timer")
+  suspend fun getTimer(
+    @Path("timerId") timerId: Int,
+  ): BaseResponse<ResponseGetTimerDto>
 }
