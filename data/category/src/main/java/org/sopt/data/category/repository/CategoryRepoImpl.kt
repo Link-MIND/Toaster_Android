972962479f01b87a910eb3d3e23698db80c12c91
Choi Min Young <codingmy@naver.com>
2024-01-16 07:02:13 +0900

    [feat] #110 repository 구현부에 추가 구현

--- data/category/src/main/java/org/sopt/data/category/repository/CategoryRepoImpl.kt
+++ data/category/src/main/java/org/sopt/data/category/repository/CategoryRepoImpl.kt
@@ -2,6 +2,8 @@ package org.sopt.data.category.repository
 
 import org.sopt.data.category.datasource.RemoteCategoryDataSource
 import org.sopt.domain.category.category.repository.CategoryRepository
+import org.sopt.model.category.CategoryDuplicate
+import org.sopt.model.category.CategoryLinkCondition
 import org.sopt.model.category.CategoryList
 import javax.inject.Inject
 
@@ -13,4 +15,16 @@ class CategoryRepoImpl @Inject constructor(
 
   override suspend fun postAddCategory(categoryTitle: String): Result<Int> =
     runCatching { remoteCategoryDataSource.postAddCategoryTitle(categoryTitle) }
-}
+
+  override suspend fun deleteCategory(deleteCategoryList: List<Long>): Result<Unit> =
+    runCatching { remoteCategoryDataSource.deleteCategory(deleteCategoryList) }
+
+    override suspend fun getCategoryDuplicate(title: String): Result<CategoryDuplicate> =
+      runCatching { remoteCategoryDataSource.getCategoryDuplicate(title) }
+
+    override suspend fun getCategoryLink(filter: String, isAllCategory: Boolean): Result<CategoryLinkCondition> =
+      runCatching { remoteCategoryDataSource.getCategoryLink(filter, isAllCategory) }
+
+    override suspend fun patchCategoryEdit(): Result<Unit> =
+      runCatching { remoteCategoryDataSource.patchCategoryEdit() }
+  }
