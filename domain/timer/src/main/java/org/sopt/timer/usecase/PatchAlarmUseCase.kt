1116dee3de10b4d9944f6de00ea70135b6119972
Sangwook123 <operawook@catholic.ac.kr>
2024-01-15 17:28:07 +0900

    [feat] #95 patch alarm, patch timer use case

---
+++ domain/timer/src/main/java/org/sopt/timer/usecase/PatchAlarmUseCase.kt
@@ -0,0 +1,10 @@
+package org.sopt.timer.usecase
+
+import org.sopt.timer.repository.TimerRepository
+import javax.inject.Inject
+
+class PatchAlarmUseCase @Inject constructor(
+  private val timerRepository: TimerRepository
+) {
+  suspend operator fun invoke(timerId: Int) = timerRepository.patchAlarm(timerId)
+}
