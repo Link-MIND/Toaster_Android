ce00105ce893fbc13a78f2278d6cc6dcf108e69f
Choi Min Young <codingmy@naver.com>
2024-01-16 01:59:11 +0900

    [feat] #110 repository 인터페이스 구현

--- domain/category/src/main/java/org/sopt/domain/category/category/repository/CategoryRepository.kt
+++ domain/category/src/main/java/org/sopt/domain/category/category/repository/CategoryRepository.kt
@@ -1,9 +1,19 @@
 package org.sopt.domain.category.category.repository
 
+import org.sopt.model.category.CategoryDuplicate
+import org.sopt.model.category.CategoryLinkGet
 import org.sopt.model.category.CategoryList
 
 interface CategoryRepository {
   suspend fun getCategoryAll(): Result<CategoryList>
 
   suspend fun postAddCategory(categoryTitle: String): Result<Int>
+
+  suspend fun deleteCategory(deleteCategoryList: List<Long>) : Result<Unit>
+
+  suspend fun getCategoryDuplicate(title: String): Result<CategoryDuplicate>
+
+  suspend fun getCategoryLink(filter: String, isAllCategory: Boolean): Result<CategoryLinkGet>
+
+  suspend fun patchCategoryEdit(): Result<Unit>
 }
