1a8c767c86a612f1b310eb03d98f5f2982a19f7e
Choi Min Young <codingmy@naver.com>
2024-01-16 07:01:17 +0900

    [feat] #110 dto 매핑함수 작성

--- data-remote/category/src/main/java/org/sopt/dataremote/category/response/ResponseCategoryDuplicateDTO.kt
+++ data-remote/category/src/main/java/org/sopt/dataremote/category/response/ResponseCategoryDuplicateDTO.kt
@@ -2,9 +2,12 @@ package org.sopt.dataremote.category.response
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.sopt.model.category.CategoryDuplicate
 
 @Serializable
-data class ResponseCategoryDuplicateDTO (
+data class ResponseCategoryDuplicateDTO(
   @SerialName("isDuplicated")
   val isDuplicated: Boolean,
 )
+
+internal fun ResponseCategoryDuplicateDTO.toCoreModel() = CategoryDuplicate(isDuplicate = this.isDuplicated)
