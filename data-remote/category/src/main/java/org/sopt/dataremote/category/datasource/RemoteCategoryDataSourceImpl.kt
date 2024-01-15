25849c940131b448891e04572390d106f230db4c
Choi Min Young <codingmy@naver.com>
2024-01-16 01:25:10 +0900

    [feat] #110 데이터소스 구현부

--- data-remote/category/src/main/java/org/sopt/dataremote/category/datasource/RemoteCategoryDataSourceImpl.kt
+++ data-remote/category/src/main/java/org/sopt/dataremote/category/datasource/RemoteCategoryDataSourceImpl.kt
@@ -2,7 +2,9 @@ package org.sopt.dataremote.category.datasource
 
 import org.sopt.data.category.datasource.RemoteCategoryDataSource
 import org.sopt.dataremote.category.api.CategoryService
+import org.sopt.dataremote.category.request.RequestCategoryDeleteDTO
 import org.sopt.dataremote.category.request.RequestCategoryTitleDto
+import org.sopt.dataremote.category.request.toRequestDTO
 import org.sopt.dataremote.category.response.toCoreModel
 import org.sopt.model.category.CategoryList
 import javax.inject.Inject
@@ -19,4 +21,25 @@ class RemoteCategoryDataSourceImpl @Inject constructor(
         categoryTitle = categoryTitle,
       ),
     ).code
+
+  override suspend fun deleteCategory(deleteCategoryList: List<Long>) {
+    categoryService.deleteCategory(
+      deleteCategoryList.toRequestDTO(),
+    )
+  }
+
+  override suspend fun getCategoryDuplicate(title: String) {
+    categoryService.getCategoryDuplicate(title = title)
+  }
+
+  override suspend fun getCategoryLink(filter: String, isAllCategory: Boolean) {
+    categoryService.getCategoryLink(
+      filter = filter,
+      isAllCategory = isAllCategory,
+    )
+  }
+
+  override suspend fun patchCategoryEdit() {
+
+  }
 }
