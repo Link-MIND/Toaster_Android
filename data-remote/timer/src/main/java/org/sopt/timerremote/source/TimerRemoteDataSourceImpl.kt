896b6f94d06f23eaeaa0bf8141e53637a06c49fb
Sangwook123 <operawook@catholic.ac.kr>
2024-01-15 17:27:06 +0900

    [feat] #95 datasource impl

--- data-remote/timer/src/main/java/org/sopt/timerremote/source/TimerRemoteDataSourceImpl.kt
+++ data-remote/timer/src/main/java/org/sopt/timerremote/source/TimerRemoteDataSourceImpl.kt
@@ -1,17 +1,25 @@
 package org.sopt.timerremote.source
 
+import org.sopt.model.timer.Timer
+import org.sopt.model.timer.TimerData
 import org.sopt.network.model.response.base.BaseResponse
-import org.sopt.timer.model.remote.request.RequestPostTimerDto
-import org.sopt.timer.model.remote.response.ResponseGetTimerPageDto
 import org.sopt.timer.source.remote.TimerRemoteDataSource
 import org.sopt.timerremote.api.TimerService
+import org.sopt.timerremote.model.request.toPatchDto
+import org.sopt.timerremote.model.request.toPostDto
+import org.sopt.timerremote.model.response.toCoreModel
 import javax.inject.Inject
 
 class TimerRemoteDataSourceImpl @Inject constructor(
   private val timerService: TimerService,
 ) : TimerRemoteDataSource {
-  override suspend fun getTimerMain(): BaseResponse<ResponseGetTimerPageDto> = timerService.getTimerMain()
-  override suspend fun postTimer(requestPostTimerDto: RequestPostTimerDto): BaseResponse<Unit> = timerService.postTimer(requestPostTimerDto)
+  override suspend fun getTimerMain(): Pair<List<Timer>, List<Timer>>? = timerService.getTimerMain().data?.toCoreModel()
+  override suspend fun postTimer(timerData: TimerData): BaseResponse<Unit> = timerService.postTimer(timerData.toPostDto())
 
   override suspend fun deleteTimer(timerId: Int): BaseResponse<Unit> = timerService.deleteTimer(timerId)
+
+  override suspend fun patchTimer(timerId: Int, timerData: TimerData): BaseResponse<Unit> =
+    timerService.patchTimer(timerId, timerData.toPatchDto())
+
+  override suspend fun patchAlarm(timerId: Int): BaseResponse<Unit> = timerService.patchAlarm(timerId)
 }
