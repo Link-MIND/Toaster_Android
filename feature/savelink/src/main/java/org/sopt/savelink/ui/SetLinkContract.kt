91e29a03a68564fbe3416d2de71b4f378bc9e3e7
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-17 02:47:39 +0900

    [feat] : #114 mvi set Link

--- feature/savelink/src/main/java/org/sopt/savelink/ui/SetLinkContract.kt
+++ feature/savelink/src/main/java/org/sopt/savelink/ui/SetLinkContract.kt
@@ -3,10 +3,14 @@ package org.sopt.savelink.ui
 import org.sopt.model.category.Category
 
 data class SetLinkState(
-  val categoryList: List<Clip> = emptyList()
+  val categoryList: List<Clip> = emptyList(),
+  val categoryId: Long = 0,
+  val url:String = ""
 ) {
 }
 sealed interface SetLinkSideEffect {
   data object NavigateSetLink : SetLinkSideEffect
   data object ShowBottomSheet : SetLinkSideEffect
+
+  data object ShowDialog : SetLinkSideEffect
 }
