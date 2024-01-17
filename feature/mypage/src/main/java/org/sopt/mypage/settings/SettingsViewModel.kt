a6a3e838ac619c4a52b9c339587b8d1e1a581e99
CHANGEL <parkchangel@naver.com>
2024-01-17 23:06:42 +0900

    [feat] : #103 settingPage api

--- feature/mypage/src/main/java/org/sopt/mypage/settings/SettingsViewModel.kt
+++ feature/mypage/src/main/java/org/sopt/mypage/settings/SettingsViewModel.kt
@@ -9,11 +9,16 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.sopt.auth.repository.AuthRepository
+import org.sopt.model.user.SettingPageData
 import org.sopt.ui.view.UiState
+import org.sopt.user.usecase.GetUserSettingUseCase
+import org.sopt.user.usecase.PatchPushUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
+  private val getUserSettingUseCase: GetUserSettingUseCase,
+  private val patchPushUseCase: PatchPushUseCase,
   private val authRepository: AuthRepository,
 ) : ViewModel() {
   private val _logoutState = MutableStateFlow<UiState<Unit>>(UiState.Empty)
@@ -21,6 +26,30 @@ class SettingsViewModel @Inject constructor(
 
   private val _withdrawState = MutableStateFlow<UiState<Unit>>(UiState.Empty)
   val withdrawState: StateFlow<UiState<Unit>> = _withdrawState.asStateFlow()
+
+  private val _settingState = MutableStateFlow<UiState<SettingPageData>>(UiState.Empty)
+  val settingState: StateFlow<UiState<SettingPageData>> = _settingState.asStateFlow()
+
+  val pushIsAllowed = MutableStateFlow(true)
+
+  fun getUserInfo() = viewModelScope.launch {
+    getUserSettingUseCase.invoke().onSuccess { data ->
+      _settingState.emit(UiState.Success(data))
+      pushIsAllowed.emit(data.fcmIsAllowed)
+      Log.d("UserSettingSuccess", "$data")
+    }.onFailure { error ->
+      Log.d("UserSetting", "$error")
+    }
+  }
+
+  fun patchPush(allowedPush: Boolean) = viewModelScope.launch {
+    patchPushUseCase.invoke(allowedPush).onSuccess { data ->
+      pushIsAllowed.emit(data)
+      Log.d("PatchPushSuccess", "$data")
+    }.onFailure {
+      Log.e("실패", it.message.toString())
+    }
+  }
 
   fun logout() = viewModelScope.launch {
     authRepository.signout().onSuccess {
