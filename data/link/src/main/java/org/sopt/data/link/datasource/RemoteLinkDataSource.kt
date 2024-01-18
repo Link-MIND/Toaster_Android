c637a7620b3f0068aa6acd7cd8bf97cae3f5fc7b
Sangwook123 <operawook@catholic.ac.kr>
2024-01-18 22:40:43 +0900

    [chore] #134 patch read link data source

--- data/link/src/main/java/org/sopt/data/link/datasource/RemoteLinkDataSource.kt
+++ data/link/src/main/java/org/sopt/data/link/datasource/RemoteLinkDataSource.kt
@@ -3,5 +3,5 @@ package org.sopt.data.link.datasource
 interface RemoteLinkDataSource {
   suspend fun postSaveLink(linkUrl: String, categoryId: Long?): Int
   suspend fun deleteLink(toastId: Long): Int
-  suspend fun patchReadLink(toastId: Long): Int
+  suspend fun patchReadLink(toastId: Long): Boolean
 }
