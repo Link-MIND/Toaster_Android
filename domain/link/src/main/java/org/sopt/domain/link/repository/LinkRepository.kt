c123a9ba356bd74ba2ac163f47dbd2313a20eccc
Sangwook123 <operawook@catholic.ac.kr>
2024-01-19 01:09:45 +0900

    [chore] #143 repository

--- domain/link/src/main/java/org/sopt/domain/link/repository/LinkRepository.kt
+++ domain/link/src/main/java/org/sopt/domain/link/repository/LinkRepository.kt
@@ -3,5 +3,5 @@ package org.sopt.domain.link.repository
 interface LinkRepository {
   suspend fun postSaveLink(linkUrl: String, categoryId: Long?): Result<Int>
   suspend fun deleteLink(toastId: Long): Result<Int>
-  suspend fun patchReadLink(toastId: Long): Result<Boolean>
+  suspend fun patchReadLink(toastId: Long, isRead: Boolean): Result<Boolean>
 }
