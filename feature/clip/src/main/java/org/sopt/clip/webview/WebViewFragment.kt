2dd831c5069aee03943b28ef42b5d3a79c436ca0
Sangwook123 <operawook@catholic.ac.kr>
2024-01-18 22:49:31 +0900

    [feat] #134 web view fragment

--- feature/clip/src/main/java/org/sopt/clip/webview/WebViewFragment.kt
+++ feature/clip/src/main/java/org/sopt/clip/webview/WebViewFragment.kt
@@ -10,26 +10,51 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import org.sopt.clip.R
 import org.sopt.clip.databinding.FragmentWebviewBinding
 import org.sopt.ui.base.BindingFragment
 import org.sopt.ui.context.hideKeyboard
+import org.sopt.ui.fragment.viewLifeCycle
+import org.sopt.ui.fragment.viewLifeCycleScope
 import org.sopt.ui.view.onThrottleClick
 
+@AndroidEntryPoint
 class WebViewFragment : BindingFragment<FragmentWebviewBinding>({ FragmentWebviewBinding.inflate(it) }) {
-
+  private val viewModel: WebViewViewModel by viewModels()
+  val args: WebViewFragmentArgs by navArgs()
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     binding.wbClip.settings.javaScriptEnabled = true
-    val args: WebViewFragmentArgs by navArgs()
-    val site = args.site
-    setupWebView(site)
+    val arg = args.site.split(",,,")
+    binding.ivRead.onThrottleClick {
+      Log.e("읽음","누름")
+      viewModel.patchReadLink(arg[1].toLong())
+    }
+
+    viewModel.patchReadLinkResult.flowWithLifecycle(viewLifeCycle).onEach {
+      when (it) {
+        true -> {
+          binding.ivRead.setImageResource(org.sopt.mainfeature.R.drawable.ic_read_after_24)
+        }
+
+        false -> {
+          binding.ivRead.setImageResource(R.drawable.ic_read_before_24)
+        }
+      }
+    }.launchIn(viewLifeCycleScope)
+    Log.e("어케나오노", arg[0])
+    setupWebView(arg[0])
     onClickClipLink()
     onClickWebViewClose()
     onClickWebViewReStart()
-    initReadBtnClickLister()
+    //initReadBtnClickLister()
     initNavigationBtnClickListener()
     initBrowserBtnClickListener()
     initEditorActionListener()
@@ -74,13 +99,14 @@ class WebViewFragment : BindingFragment<FragmentWebviewBinding>({ FragmentWebvie
 
   private fun initReadBtnClickLister() {
     with(binding) {
-      ivReadBefore.onThrottleClick {
-        handleVisibility(ivReadBefore, ivReadAfter)
+      ivRead.onThrottleClick {
+
+        //handleVisibility(ivRead, ivReadAfter)
       }
 
-      ivReadAfter.onThrottleClick {
-        handleVisibility(ivReadAfter, ivReadBefore)
-      }
+      /*ivReadAfter.onThrottleClick {
+        handleVisibility(ivReadAfter, ivRead)
+      }*/
     }
   }
 
