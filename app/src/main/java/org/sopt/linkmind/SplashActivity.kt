3eafad0c50f5bc1d4f4e588252a850b2c7541a23
Choi Min Young <codingmy@naver.com>
2024-01-18 17:44:21 +0900

    [feat] #135 theme 구현 적용

--- app/src/main/java/org/sopt/linkmind/SplashActivity.kt
+++ app/src/main/java/org/sopt/linkmind/SplashActivity.kt
@@ -1,12 +1,14 @@
 package org.sopt.linkmind
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import org.sopt.login.onboarding.LoginActivity
 
-class SplashActivity : Activity() {
+class SplashActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
     super.onCreate(savedInstanceState)
     val intent = Intent(this, LoginActivity::class.java)
     startActivity(intent)
