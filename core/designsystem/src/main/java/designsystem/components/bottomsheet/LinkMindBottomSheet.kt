4311658accfe265b05fc8c47956295cea510c700
lsakee <93514333+lsakee@users.noreply.github.com>
2024-01-19 16:51:10 +0900

    [refactor] : #146 showSnackBar

--- core/designsystem/src/main/java/designsystem/components/bottomsheet/LinkMindBottomSheet.kt
+++ core/designsystem/src/main/java/designsystem/components/bottomsheet/LinkMindBottomSheet.kt
@@ -43,7 +43,6 @@ class LinkMindBottomSheet(context: Context) {
       }
     }
   }
-
   fun setBottomSheetHint(@StringRes textId: Int) {
     binding.etvBottomSheet.editText.setHint(textId)
   }
@@ -69,11 +68,9 @@ class LinkMindBottomSheet(context: Context) {
   }
 
   private fun isTextLongEnough() = binding.etvBottomSheet.editText.text.length > 1
-
   fun setTitle(@StringRes textId: Int) {
     binding.tvBottomSheetTitle.setText(textId)
   }
-
   fun showErrorMsg(): Boolean = binding.etvBottomSheet.editText.text.length > 15 || binding.etvBottomSheet.editText.text.isEmpty()
 
   fun setErroMsg(@StringRes textId: Int) {
