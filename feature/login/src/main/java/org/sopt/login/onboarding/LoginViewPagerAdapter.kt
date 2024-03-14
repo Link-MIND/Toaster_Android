2ff8fa52f35f338a88550000324bc28d7ffe5b67
Sangwook123 <operawook@catholic.ac.kr>
2024-03-14 19:35:55 +0900

    [feat] #160 뷰페이저 어댑터

---
+++ feature/login/src/main/java/org/sopt/login/onboarding/LoginViewPagerAdapter.kt
@@ -0,0 +1,35 @@
+package org.sopt.login.onboarding
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.login.R
+
+class LoginViewPagerAdapter(private val context: Context) : RecyclerView.Adapter<LoginViewPagerAdapter.ItemViewHolder>() {
+
+  private val layoutResIds = listOf(
+    R.layout.item_onboarding_first,
+    R.layout.item_onboarding_second,
+    R.layout.item_onboarding_third,
+    R.layout.item_onboarding_forth
+  )
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+    val itemView = LayoutInflater.from(context).inflate(layoutResIds[viewType], parent, false)
+    return ItemViewHolder(itemView)
+  }
+
+  override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+  }
+
+  override fun getItemCount() = layoutResIds.size
+
+  override fun getItemViewType(position: Int): Int {
+    return position
+  }
+
+  inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+  }
+}
