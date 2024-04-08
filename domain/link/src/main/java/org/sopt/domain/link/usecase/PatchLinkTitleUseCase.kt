e17d84041d4c1c49dec6daf2d2046eee528890db
Sangwook123 <operawook@catholic.ac.kr>
2024-04-08 21:17:15 +0900

    [chore] #164 ktlintformat

--- domain/link/src/main/java/org/sopt/domain/link/usecase/PatchLinkTitleUseCase.kt
+++ domain/link/src/main/java/org/sopt/domain/link/usecase/PatchLinkTitleUseCase.kt
@@ -4,7 +4,7 @@ import org.sopt.domain.link.repository.LinkRepository
 import javax.inject.Inject
 
 class PatchLinkTitleUseCase @Inject constructor(
-  private val linkRepository: LinkRepository
+  private val linkRepository: LinkRepository,
 ) {
   suspend operator fun invoke(param: Param): Result<String> = linkRepository.patchLinkTitle(
     toastId = param.toastId,
