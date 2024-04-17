88a11d7f1c8e9d0bb0b27b3ca0641d0506ad7c83
Sangwook123 <operawook@catholic.ac.kr>
2024-04-18 04:24:47 +0900

    [feat] #165 clip adapter, viewholder

---
+++ feature/share/src/main/java/org/sopt/share/viewholder/ClipSelectViewHolder.kt
@@ -0,0 +1,59 @@
+package org.sopt.share.viewholder
+
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.share.databinding.ItemTimerClipSelectBinding
+import org.sopt.share.model.Clip
+import org.sopt.ui.view.onThrottleClick
+
+class ClipSelectViewHolder(
+  val binding: ItemTimerClipSelectBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+  fun onBind(data: Clip?, position: Int, onClickClip: (Clip, Int) -> Unit) {
+    if (data == null) return
+    setImage(position)
+    setTexts(data)
+    setTextColor(data, position)
+    setOnClickListener(data, onClickClip)
+  }
+
+  private fun setImage(position: Int) {
+    val imageResource = if (position == 0) {
+      org.sopt.mainfeature.R.drawable.ic_clip_all_24
+    } else {
+      org.sopt.mainfeature.R.drawable.ic_clip_24
+    }
+    binding.ivItemTimerClip.setImageResource(imageResource)
+  }
+
+  private fun setTexts(data: Clip) {
+    with(binding) {
+      tvItemTimerClipName.text = data.categoryTitle
+      tvItemTimerClipCount.text = "${data.toastNum}개"
+    }
+  }
+
+  private fun setTextColor(data: Clip, position: Int) {
+    val colorResource = if (data.isSelected) {
+      setClipImageResource(position, org.sopt.mainfeature.R.drawable.ic_clip_all_24_primary, org.sopt.mainfeature.R.drawable.ic_clip_24_primary)
+      org.sopt.mainfeature.R.color.primary
+    } else {
+      setClipImageResource(position, org.sopt.mainfeature.R.drawable.ic_clip_all_24, org.sopt.mainfeature.R.drawable.ic_clip_24)
+      org.sopt.mainfeature.R.color.neutrals900
+    }
+    val color = ContextCompat.getColor(binding.root.context, colorResource)
+    binding.tvItemTimerClipCount.setTextColor(color)
+    binding.tvItemTimerClipName.setTextColor(color)
+  }
+
+  private fun setClipImageResource(position: Int, selectedImage: Int, unselectedImage: Int) {
+    val imageResource = if (position == 0) selectedImage else unselectedImage
+    binding.ivItemTimerClip.setImageResource(imageResource)
+  }
+
+  private fun setOnClickListener(data: Clip, onClick: (Clip, Int) -> Unit) {
+    binding.root.onThrottleClick {
+      onClick(data, bindingAdapterPosition)
+    }
+  }
+}
