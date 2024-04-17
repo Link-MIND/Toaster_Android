75c5af263d98bea65f9517bafa98d07c8fe8fd4c
Sangwook123 <operawook@catholic.ac.kr>
2024-04-18 04:25:39 +0900

    [feat] #165 clip data class

---
+++ feature/share/src/main/java/org/sopt/share/model/Clip.kt
@@ -0,0 +1,17 @@
+package org.sopt.share.model
+
+import org.sopt.model.category.Category
+
+data class Clip(
+  val categoryId: Long?,
+  val categoryTitle: String,
+  val toastNum: Long,
+  var isSelected: Boolean,
+)
+
+internal fun Category.toModel() = Clip(
+  categoryId = categoryId,
+  categoryTitle = categoryTitle ?: "",
+  toastNum = toastNum,
+  isSelected = false,
+)
